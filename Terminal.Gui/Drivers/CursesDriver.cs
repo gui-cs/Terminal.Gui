d50687519efedd3084afeaf881940cd64c01896d
BDisp <bd.bdisp@gmail.com>
2020-05-14 19:42:41 +0100

    Revert "Updates screen on Unix window resizing."
    This reverts commit 55b64b01e83a7ebfedf0be67b6f685962598bc82.

--- Terminal.Gui/Drivers/CursesDriver.cs
+++ Terminal.Gui/Drivers/CursesDriver.cs
@@ -245,8 +245,7 @@ namespace Terminal.Gui {
 			// Note: Curses doesn't support keydown/up events and thus any passed keyDown/UpHandlers will never be called
 			Curses.timeout (-1);
 
-			(mainLoop.Driver as Mono.Terminal.UnixMainLoop).AddWatch (0, Mono.Terminal.UnixMainLoop.Condition.PollIn |
-				Mono.Terminal.UnixMainLoop.Condition.PollOut, x => {
+			(mainLoop.Driver as Mono.Terminal.UnixMainLoop).AddWatch (0, Mono.Terminal.UnixMainLoop.Condition.PollIn, x => {
 				ProcessInput (keyHandler, mouseHandler);
 				return true;
 			});
