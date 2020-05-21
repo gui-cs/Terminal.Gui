b4eb00e7602618940a3c9bd9cbd93cf28b66ed92
Charlie Kindel <charlie@kindel.com>
2020-05-21 08:18:20 -0600

    updated to also demo Activity mode

--- UICatalog/Scenarios/Progress.cs
+++ UICatalog/Scenarios/Progress.cs
@@ -9,14 +9,22 @@ namespace UICatalog {
 	[ScenarioCategory ("Controls")]
 	class Progress : Scenario {
 
-		private ProgressBar _progressBar;
+		private ProgressBar _activityProgressBar;
+		private ProgressBar _pulseProgressBar;
 		public override void Setup ()
 		{
 			Win.Add (new Button ("Start") {
 				X = Pos.Center () - 20,
 				Y = Pos.Center () - 5,
 				Clicked = () => Start ()
-			}); ;
+			});
+
+			Win.Add (new Button ("Pulse") {
+				X = Pos.Center () - 5,
+				Y = Pos.Center () - 5,
+				Clicked = () => Pulse ()
+			}); 
+
 
 			Win.Add (new Button ("Stop") {
 				X = Pos.Center () + 10,
@@ -24,24 +32,44 @@ namespace UICatalog {
 				Clicked = () => Stop()
 			});
 
-			_progressBar = new ProgressBar () {
+			_activityProgressBar = new ProgressBar () {
 				X = Pos.Center (),
 				// BUGBUG: If you remove the +1 below the control is drawn at top?!?!
 				Y = Pos.Center ()+1,
 				Width = 30,
 				Fraction = 0.25F,
 			};
-			Win.Add (_progressBar);
+			Win.Add (_activityProgressBar);
+
+			_pulseProgressBar = new ProgressBar () {
+				X = Pos.Center (),
+				// BUGBUG: If you remove the +1 below the control is drawn at top?!?!
+				Y = Pos.Center () + 3,
+				Width = 30,
+			};
+			Win.Add (_pulseProgressBar);
+		}
+
+		private void Pulse ()
+		{
+			if (_activityProgressBar.Fraction + 0.1F >= 1) {
+				_activityProgressBar.Fraction = 0F;
+			} else {
+				_activityProgressBar.Fraction += 0.1F;
+			}
+			_pulseProgressBar.Pulse ();
 		}
 
 		private void Start ()
 		{
-			_progressBar.Fraction = 0F;
+			_activityProgressBar.Fraction = 0F;
+			_pulseProgressBar.Fraction = 0F;
 		}
 
 		private void Stop ()
 		{
-			_progressBar.Fraction = 1F;
+			_activityProgressBar.Fraction = 1F;
+			_pulseProgressBar.Fraction = 1F;
 		}
 	}
 }
