3a0fe27298d467684a0e288fc9643ac5b96d77ff
Charlie Kindel <tig@users.noreply.github.com>
2020-06-03 01:59:44 -0600

    tweaked UI

--- UICatalog/Scenarios/CharacterMap.cs
+++ UICatalog/Scenarios/CharacterMap.cs
@@ -16,43 +16,40 @@ namespace UICatalog {
 	class CharacterMap : Scenario {
 		public override void Setup ()
 		{
-			var charMap = new CharMap () { X = 1, Y = 1, Width = Dim.Percent(75), Height = 16, Start = 0x2500 };
+			var charMap = new CharMap () { X = 0, Y = 0, Width = CharMap.RowWidth + 2, Height = Dim.Fill(), Start = 0x2500, 
+				ColorScheme = Colors.Dialog};
 
 			Win.Add (charMap);
 
-			var button = new Button ("Box Drawing & Geometric Shapes U+2500-25ff") { X = Pos.X (charMap), Y = Pos.Bottom (charMap) + 1,
-				Clicked = () => {
-					charMap.Start = 0x2500;
-				}
+			Button CreateBlock(Window win, ustring title, int start, int end, View align)
+			{
+				var button = new Button ($"{title} (U+{start:x5}-{end:x5})") {
+					X = Pos.X (align),
+					Y = Pos.Bottom (align),
+					Clicked = () => {
+						charMap.Start = start;
+					},
+				};
+				win.Add (button);
+				return button;
 			};
-			Win.Add (button);
 
-			button = new Button ("Dingbats U+2700-27ff") {
-				X = Pos.X (charMap),
-				Y = Pos.Bottom (button),
-				Clicked = () => {
-					charMap.Start = 0x2700;
-				}
-			};
-			Win.Add (button);
-
-			button = new Button ("Miscellaneous Symbols and Arrows U+2b00-2bff") {
-				X = Pos.X (charMap),
-				Y = Pos.Bottom (button),
-				Clicked = () => {
-					charMap.Start = 0x2b00;
-				}
-			};
-			Win.Add (button);
-
-			button = new Button ("Arrows U+2190-21ff") {
-				X = Pos.X (charMap),
-				Y = Pos.Bottom (button),
-				Clicked = () => {
-					charMap.Start = 0x2190;
-				}
-			};
-			Win.Add (button);
+			var label = new Label ("Unicode Blocks:") { X = Pos.Right (charMap) + 2, Y = Pos.Y (charMap) };
+			Win.Add (label);
+			var button = CreateBlock (Win, "Currency Symbols", 0x20A0, 0x20CF, label);
+			button = CreateBlock (Win, "Letterlike Symbols", 0x2100, 0x214F, button);
+			button = CreateBlock (Win, "Arrows", 0x2190, 0x21ff, button);
+			button = CreateBlock (Win, "Mathematical symbols", 0x2200, 0x22ff, button);
+			button = CreateBlock (Win, "Miscellaneous Technical", 0x2300, 0x23ff, button);
+			button = CreateBlock (Win, "Box Drawing & Geometric Shapes", 0x2500, 0x25ff, button);
+			button = CreateBlock (Win, "Miscellaneous Symbols", 0x2600, 0x26ff, button);
+			button = CreateBlock (Win, "Dingbats", 0x2700, 0x27ff, button);
+			button = CreateBlock (Win, "Braille", 0x2800, 0x28ff, button);
+			button = CreateBlock (Win, "Miscellaneous Symbols and Arrows", 0x2b00, 0x2bff, button);
+			button = CreateBlock (Win, "Alphabetic Presentation Forms", 0xFB00, 0xFb4f, button);
+			button = CreateBlock (Win, "Cuneiform Numbers and Punctuation[1", 0x12400, 0x1240f, button);
+			button = CreateBlock (Win, "Chess Symbols", 0x1FA00, 0x1FA0f, button);
+			button = CreateBlock (Win, "End", CharMap.MaxCodePointVal - 16, CharMap.MaxCodePointVal, button);
 		}
 	}
 
@@ -73,13 +70,15 @@ namespace UICatalog {
 		}
 		int _start = 0x2500;
 
+		public static int MaxCodePointVal => 0xE0FFF;
+
 		// Row Header + space + (space + char + space)
-		public static int RowHeaderWidth => "U+000x".Length;
+		public static int RowHeaderWidth => $"U+{MaxCodePointVal:x5}".Length;
 		public static int RowWidth => RowHeaderWidth + 1 + (" c ".Length * 16);
 
 		public CharMap ()
 		{
-			ContentSize = new Size (CharMap.RowWidth, int.MaxValue / 16);
+			ContentSize = new Size (CharMap.RowWidth, MaxCodePointVal / 16);
 			ShowVerticalScrollIndicator = true;
 			ShowHorizontalScrollIndicator = false;
 			LayoutComplete += (sender, args) => {
@@ -101,12 +100,15 @@ namespace UICatalog {
 				Driver.AddStr ($" {header:x} ");
 			}
 			for (int row = 0; row < viewport.Height - 1; row++) {
-				var rowLabel = $"U+{(((-viewport.Y + row) * 16)) / 16:x}x";
-				Move (0, row + 1);
-				Driver.AddStr (rowLabel);
-				for (int col = 0; col < 16; col++) {
-					Move (viewport.X + (RowHeaderWidth + 1 + (col * 3)), 0 + row + 1);
-					Driver.AddStr ($" {(char)(((-viewport.Y + row) * 16) + col)} ");
+				int val = (-viewport.Y + row) * 16;
+				if (val < MaxCodePointVal) {
+					var rowLabel = $"U+{val / 16:x4}x";
+					Move (0, row + 1);
+					Driver.AddStr (rowLabel);
+					for (int col = 0; col < 16; col++) {
+						Move (viewport.X + RowHeaderWidth + 1 + (col * 3), 0 + row + 1);
+						Driver.AddStr ($" {(char)((-viewport.Y + row) * 16 + col)} ");
+					}
 				}
 			}
 		}
