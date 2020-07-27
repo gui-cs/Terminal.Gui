ffef3276e42c8a99727618aeebbdf1fa6cb37ed0
BDisp <bd.bdisp@gmail.com>
2020-07-27 22:10:04 +0100

    Fixes #829. Hangs with disabled menu item selected solved.

--- Terminal.Gui/Core/View.cs
+++ Terminal.Gui/Core/View.cs
@@ -1911,6 +1911,9 @@ namespace Terminal.Gui {
 				Remove (subview);
 				subview.Dispose ();
 			}
+			if (Application.Top.focused == this) {
+				Application.Top.focused = null;
+			}
 			base.Dispose (disposing);
 		}
 
