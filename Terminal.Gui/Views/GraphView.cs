9f835b0cb82c32aaacace4185935f407045daa30
Thomas Nind <TZNind@dundee.ac.uk>
2021-04-05 10:40:19 +0100

    Tests and scrolling

--- Terminal.Gui/Views/GraphView.cs
+++ Terminal.Gui/Views/GraphView.cs
@@ -18,21 +18,22 @@ namespace Terminal.Gui {
 		public List<ISeries> Series {get;} = new List<ISeries>();
 
 		/// <summary>
-		/// The data space position of the bottom left of the control.
+		/// The graph space position of the bottom left of the control.
 		/// Changing this scrolls the viewport around in the graph
 		/// </summary>
 		/// <value></value>
-		public PointF Origin {get;set;} = new PointF(0,0);
+		public PointF ScrollOffset {get;set;} = new PointF(0,0);
 
 		/// <summary>
-		/// Translates console width/height into data space. Defaults
-		/// to 1 row/col of console space being 1 unit of data space.		/// 
+		/// Translates console width/height into graph space. Defaults
+		/// to 1 row/col of console space being 1 unit of graph space. 
 		/// </summary>
 		/// <returns></returns>
 		public PointF CellSize {get;set;} = new PointF(1,1);
 
 		public GraphView()
 		{
+			CanFocus = true;
 
 			AxisX = new AxisView(this,Orientation.Horizontal);
 		 	AxisY = new AxisView(this,Orientation.Vertical);
@@ -44,17 +45,24 @@ namespace Terminal.Gui {
 		///<inheritdoc/>
 		public override void Redraw (Rect bounds)
 		{
+			Driver.SetAttribute (ColorScheme.Normal);
+
+			Move (0, 0);
+
+			// clear all old content
+			for (int i = 0; i < Bounds.Height; i++) {
+				Move (0, i);
+				Driver.AddStr (new string (' ', Bounds.Width));
+			}
+
 			base.Redraw(bounds);
 
 			RefreshViewport();
-
-			Move (0, 0);
-			Driver.SetAttribute (ColorScheme.Normal);
-
+						
 			for(int x=0;x<Bounds.Width;x++){
 				for(int y=0;y<Bounds.Width;y++){
 
-					var space = ViewToDataSpace(x,y);
+					var space = ScreenToGraphSpace(x,y);
 
 					foreach(var s in Series){
 						var rune = s.GetCellValueIfAny(space);
@@ -70,16 +78,34 @@ namespace Terminal.Gui {
 		}
 
 		/// <summary>
-		/// Projects the given screen location (in client area) into the 
-		/// data space (coordinate system of the <see cref="Series"/>)
+		/// Returns the section of the graph that is represented by the given
+		/// screen position
 		/// </summary>
 		/// <param name="col"></param>
 		/// <param name="row"></param>
 		/// <returns></returns>
-		public RectangleF ViewToDataSpace (int col, int row)
+		public RectangleF ScreenToGraphSpace (int col, int row)
 		{
-			// TODO: fix this to respect Zoom etc
-			return new RectangleF(col * CellSize.X, (Bounds.Height - row)*CellSize.Y ,CellSize.X,CellSize.Y);
+			return new RectangleF (
+				ScrollOffset.X + (col * CellSize.X),
+				ScrollOffset.Y + ((Bounds.Height - row) * CellSize.Y),
+				CellSize.X, CellSize.Y);
+		}
+
+		/// <summary>
+		/// Calculates the screen location for a given point in graph space.
+		/// Bear in mind these This may not be off screen
+		/// </summary>
+		/// <param name="location">Point within the graph</param>
+		/// <returns>Screen position (Row / Column) which would be used to render the <paramref name="location"/></returns>
+		public Point GraphSpaceToScreen (PointF location)
+		{
+			return new Point (
+				
+				(int)((location.X - ScrollOffset.X) / CellSize.X),
+				 // screen coordinates are top down while graph coordinates are bottom up
+				 Bounds.Height - (int)((location.Y - ScrollOffset.Y) / CellSize.Y) 
+				);
 		}
 
 		/// <summary>
@@ -88,19 +114,27 @@ namespace Terminal.Gui {
 		/// </summary>
 		public void RefreshViewport(){
 
-			// TODO: Consider Origin
+			// find the origin of the graph in screen space (this allows for 'crosshair' style
+			// graphs where positive and negative numbers visible
+			var origin = GraphSpaceToScreen (new PointF (0, 0));
 
 			// position X axis
 			AxisX.X = 0;
-			AxisX.Y = Bounds.Height -1;
-			AxisX.Height = 1;
+
+			// Float the X axis so that it accurately represents the origin of the graph
+			// but anchor it to top/bottom if the origin is offscreen
+			AxisX.Y = Math.Min(Math.Max(0, origin.Y), Bounds.Height);
+			AxisX.Height = AxisX.Thickness;
 			AxisX.Width = Bounds.Width;
 
 			// position Y axis
-			AxisY.X = 0;
+
+			// Float the Y axis so that it accurately represents the origin of the graph
+			// but anchor it to left/right if the origin is offscreen
+			AxisY.X = Math.Min (Math.Max (0, origin.X), Bounds.Width - AxisX.Thickness);
 			AxisY.Y = 0;
 			AxisY.Height = Bounds.Height;
-			AxisY.Width = 1;
+			AxisY.Width = AxisY.Thickness;
 		}
 
 		/// <inheritdoc/>
@@ -112,13 +146,37 @@ namespace Terminal.Gui {
 				switch (keyEvent.Key) {
 
 				case Key.CursorLeft:
+					Scroll (-CellSize.X, 0);
 					return true;
 				case Key.CursorRight:
+					Scroll (CellSize.X, 0);
+					return true;
+				case Key.CursorDown:
+					Scroll (0, -CellSize.Y);
+					return true;
+				case Key.CursorUp:
+					Scroll (0,CellSize.Y);
 					return true;
 				}
 			}
 
 			return base.ProcessKey (keyEvent);
+		}
+
+		/// <summary>
+		/// Scrolls the view by a given number of units in graph space.
+		/// See <see cref="CellSize"/> to translate this into rows/cols
+		/// </summary>
+		/// <param name="offsetX"></param>
+		/// <param name="offsetY"></param>
+		private void Scroll (float offsetX, float offsetY)
+		{
+			ScrollOffset = new PointF (
+				ScrollOffset.X + offsetX,
+				ScrollOffset.Y + offsetY);
+
+			RefreshViewport ();
+			SetNeedsDisplay ();
 		}
 	}
 
@@ -131,8 +189,8 @@ namespace Terminal.Gui {
 		/// Return the rune that should be drawn on the screen (if any)
 		/// for the current position in the control
 		/// </summary>
-		/// <param name="dataSpace">Projection of the screen location into the chart data space</param>
-		Rune? GetCellValueIfAny(RectangleF dataSpace);
+		/// <param name="graphSpace">Projection of the screen location into the chart graph space</param>
+		Rune? GetCellValueIfAny(RectangleF graphSpace);
 	}
 
 	public class ScatterSeries : ISeries
@@ -143,9 +201,9 @@ namespace Terminal.Gui {
 		/// <returns></returns>
 		public List<PointF> Points {get;set;} = new List<PointF>();
 
-		public Rune? GetCellValueIfAny (RectangleF dataSpace)
+		public Rune? GetCellValueIfAny (RectangleF graphSpace)
 		{
-			if(Points.Any(p=>dataSpace.Contains(p))){
+			if(Points.Any(p=>graphSpace.Contains(p))){
 				return 'x';
 			}
 
@@ -171,7 +229,7 @@ namespace Terminal.Gui {
 		public bool ShowLabels {get;set;}
 
 		/// <summary>
-		/// Number of units of data space between ticks on axis
+		/// Number of units of graph space between ticks on axis
 		/// </summary>
 		/// <value></value>
 		public float Increment {get;set;} = 1;
@@ -181,6 +239,13 @@ namespace Terminal.Gui {
 		/// </summary>
 		/// <value></value>
 		public GraphView Graph {get;}
+
+		/// <summary>
+		/// The amount of screen space required to render the line and labels.
+		/// This is a measure perpendicular to the <see cref="Orientation"/>
+		/// (i.e. it is height of a Horizontal axis and width of a Vertical axis)
+		/// </summary>
+		public int Thickness { get; } = 1;
 
 		public AxisView()
 		{
@@ -216,7 +281,7 @@ namespace Terminal.Gui {
 				for(int i=0;i<bounds.Width;i++){
 					Move (i,0);
 
-					var rect = Graph.ViewToDataSpace (i, 0);
+					var rect = Graph.ScreenToGraphSpace (i, 0);
 					
 					if(rect.X>= nextTick && nextTick <= rect.Right)
 					{
@@ -238,7 +303,7 @@ namespace Terminal.Gui {
 				for(int i= bounds.Height; i>0;i--){
 					Move (0, i);
 
-					var rect = Graph.ViewToDataSpace (0, i);
+					var rect = Graph.ScreenToGraphSpace (0, i);
 
 					if (rect.Y >= nextTick && nextTick <= rect.Bottom) {
 						Driver.AddRune (tickSymbol);
---
+++ UnitTests/GraphViewTests.cs
@@ -0,0 +1,166 @@
+﻿using System.Drawing;
+using Terminal.Gui;
+using Xunit;
+
+namespace UnitTests {
+	public class GraphViewTests {
+
+		#region Screen to Graph Tests
+
+		[Fact]
+		public void ScreenToGraphSpace_DefaultCellSize()
+		{
+			var gv = new GraphView ();
+			gv.Bounds = new Rect(0,0,20,10);
+
+			// origin should be bottom left
+			var botLeft = gv.ScreenToGraphSpace (0, 10);
+			Assert.Equal (0, botLeft.X);
+			Assert.Equal (0, botLeft.Y);
+			Assert.Equal (1, botLeft.Width);
+			Assert.Equal (1, botLeft.Height);
+			
+
+			// up 2 rows of the console and along 1 col
+			var up2along1 = gv.ScreenToGraphSpace (1, 8);
+			Assert.Equal (1, up2along1.X);
+			Assert.Equal (2, up2along1.Y);
+		}
+
+		[Fact]
+		public void ScreenToGraphSpace_CustomCellSize ()
+		{
+			var gv = new GraphView ();
+			gv.Bounds = new Rect (0, 0, 20, 10);
+
+			// Each cell of screen measures 5 units in graph data model vertically and 1/4 horizontally
+			gv.CellSize = new PointF (0.25f, 5);
+
+			// origin should be bottom left
+			var botLeft = gv.ScreenToGraphSpace (0, 10);
+			Assert.Equal (0, botLeft.X);
+			Assert.Equal (0, botLeft.Y);
+			Assert.Equal (0.25, botLeft.Width);
+			Assert.Equal (5, botLeft.Height);
+
+			// up 2 rows of the console and along 1 col
+			var up2along1 = gv.ScreenToGraphSpace (1, 8);
+			Assert.Equal (0.25, up2along1.X);
+			Assert.Equal (10, up2along1.Y);
+			Assert.Equal (0.25, botLeft.Width);
+			Assert.Equal (5, botLeft.Height);
+		}
+
+		#endregion
+
+		#region Graph to Screen Tests
+
+		[Fact]
+		public void GraphSpaceToScreen_DefaultCellSize ()
+		{
+			var gv = new GraphView ();
+			gv.Bounds = new Rect (0, 0, 20, 10);
+
+			// origin should be bottom left
+			var botLeft = gv.GraphSpaceToScreen (new PointF (0, 0));
+			Assert.Equal (0, botLeft.X);
+			Assert.Equal (10, botLeft.Y); // row 10 of the view is the bottom left
+
+			// along 2 and up 1 in graph space
+			var along2up1 = gv.GraphSpaceToScreen (new PointF (2, 1));
+			Assert.Equal (2, along2up1.X);
+			Assert.Equal (9, along2up1.Y);
+		}
+
+		[Fact]
+		public void GraphSpaceToScreen_ScrollOffset ()
+		{
+			var gv = new GraphView ();
+			gv.Bounds = new Rect (0, 0, 20, 10);
+
+			//graph is scrolled to present chart space -5 to 5 in both axes
+			gv.ScrollOffset = new PointF (-5, -5);
+
+			// origin should be right in the middle of the control
+			var botLeft = gv.GraphSpaceToScreen (new PointF (0, 0));
+			Assert.Equal (5, botLeft.X);
+			Assert.Equal (5, botLeft.Y);
+
+			// along 2 and up 1 in graph space
+			var along2up1 = gv.GraphSpaceToScreen (new PointF (2, 1));
+			Assert.Equal (7, along2up1.X);
+			Assert.Equal (4, along2up1.Y);
+		}
+		[Fact]
+		public void GraphSpaceToScreen_CustomCellSize ()
+		{
+			var gv = new GraphView ();
+			gv.Bounds = new Rect (0, 0, 20, 10);
+
+			// Each cell of screen is responsible for rendering 5 units in graph data model
+			// vertically and 1/4 horizontally
+			gv.CellSize = new PointF (0.25f, 5);
+
+			// origin should be bottom left
+			var botLeft = gv.GraphSpaceToScreen (new PointF (0, 0));
+			Assert.Equal (0, botLeft.X);
+			Assert.Equal (10, botLeft.Y); // row 10 of the view is the bottom left
+
+			// along 2 and up 1 in graph space
+			var along2up1 = gv.GraphSpaceToScreen (new PointF (2, 1));
+			Assert.Equal (8, along2up1.X);
+			Assert.Equal (10, along2up1.Y);
+
+			// Y value 4 should be rendered in bottom most row
+			Assert.Equal (10, gv.GraphSpaceToScreen(new PointF (2, 4)).Y);
+			
+			// Cell height is 5 so this is the first point of graph space that should
+			// be rendered in the graph in next row up (row 9)
+			Assert.Equal (9, gv.GraphSpaceToScreen (new PointF (2, 5)).Y);
+			
+			// More boundary testing for this cell size
+			Assert.Equal (9, gv.GraphSpaceToScreen (new PointF (2, 6)).Y);
+			Assert.Equal (9, gv.GraphSpaceToScreen (new PointF (2, 7)).Y);
+			Assert.Equal (9, gv.GraphSpaceToScreen (new PointF (2, 8)).Y);
+			Assert.Equal (9, gv.GraphSpaceToScreen (new PointF (2, 9)).Y);
+			Assert.Equal (8, gv.GraphSpaceToScreen (new PointF (2, 10)).Y);
+			Assert.Equal (8, gv.GraphSpaceToScreen (new PointF (2, 11)).Y);
+		}
+
+
+		[Fact]
+		public void GraphSpaceToScreen_CustomCellSize_WithScrollOffset ()
+		{
+			var gv = new GraphView ();
+			gv.Bounds = new Rect (0, 0, 20, 10);
+
+			// Each cell of screen is responsible for rendering 5 units in graph data model
+			// vertically and 1/4 horizontally
+			gv.CellSize = new PointF (0.25f, 5);
+
+			//graph is scrolled to present some negative chart (4 negative cols and 2 negative rows)
+			gv.ScrollOffset = new PointF (-1, -10);
+
+			// origin should be in the lower left (but not right at the bottom)
+			var botLeft = gv.GraphSpaceToScreen (new PointF (0, 0));
+			Assert.Equal (4, botLeft.X);
+			Assert.Equal (8, botLeft.Y);
+
+			// along 2 and up 1 in graph space
+			var along2up1 = gv.GraphSpaceToScreen (new PointF (2, 1));
+			Assert.Equal (12, along2up1.X);
+			Assert.Equal (8, along2up1.Y);
+
+
+			// More boundary testing for this cell size/offset
+			Assert.Equal (7, gv.GraphSpaceToScreen (new PointF (2, 6)).Y);
+			Assert.Equal (7, gv.GraphSpaceToScreen (new PointF (2, 7)).Y);
+			Assert.Equal (7, gv.GraphSpaceToScreen (new PointF (2, 8)).Y);
+			Assert.Equal (7, gv.GraphSpaceToScreen (new PointF (2, 9)).Y);
+			Assert.Equal (6, gv.GraphSpaceToScreen (new PointF (2, 10)).Y);
+			Assert.Equal (6, gv.GraphSpaceToScreen (new PointF (2, 11)).Y);
+		}
+
+		#endregion
+	}
+}
