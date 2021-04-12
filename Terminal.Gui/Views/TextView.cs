2fc4f44b7c5e13f8a538df8c7bd1203aa591d7d6
BDisp <bd.bdisp@gmail.com>
2021-04-12 23:19:52 +0100

    Added missing parameters documentation.

--- Terminal.Gui/Views/TextView.cs
+++ Terminal.Gui/Views/TextView.cs
@@ -1307,6 +1307,7 @@ namespace Terminal.Gui {
 		/// <param name="textToFind">The text to find.</param>
 		/// <param name="gaveFullTurn"><c>true</c>If all the text was forward searched.<c>false</c>otherwise.</param>
 		/// <param name="matchCase">The match case setting.</param>
+		/// <param name="matchWholeWord">The match whole word setting.</param>
 		/// <param name="textToReplace">The text to replace.</param>
 		/// <param name="replace"><c>true</c>If is replacing.<c>false</c>otherwise.</param>
 		/// <returns><c>true</c>If the text was found.<c>false</c>otherwise.</returns>
@@ -1331,6 +1332,7 @@ namespace Terminal.Gui {
 		/// <param name="textToFind">The text to find.</param>
 		/// <param name="gaveFullTurn"><c>true</c>If all the text was backward searched.<c>false</c>otherwise.</param>
 		/// <param name="matchCase">The match case setting.</param>
+		/// <param name="matchWholeWord">The match whole word setting.</param>
 		/// <param name="textToReplace">The text to replace.</param>
 		/// <param name="replace"><c>true</c>If the text was found.<c>false</c>otherwise.</param>
 		/// <returns><c>true</c>If the text was found.<c>false</c>otherwise.</returns>
@@ -1362,6 +1364,7 @@ namespace Terminal.Gui {
 		/// </summary>
 		/// <param name="textToFind">The text to find.</param>
 		/// <param name="matchCase">The match case setting.</param>
+		/// <param name="matchWholeWord">The match whole word setting.</param>
 		/// <param name="textToReplace">The text to replace.</param>
 		/// <returns><c>true</c>If the text was found.<c>false</c>otherwise.</returns>
 		public bool ReplaceAllText (ustring textToFind, bool matchCase = false, bool matchWholeWord = false,
