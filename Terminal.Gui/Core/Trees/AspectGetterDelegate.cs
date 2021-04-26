5abc0ff682b43b5a8861ac18d3c3e71aa0d9bdf4
Thomas Nind <TZNind@dundee.ac.uk>
2021-04-26 09:37:24 +0100

    Extracted TreeView classes (builders, delegates, event args etc) into seperate files in namespace Terminal.Gui.Trees

---
+++ Terminal.Gui/Core/Trees/AspectGetterDelegate.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Terminal.Gui.Trees {
+
+	/// <summary>
+	/// Delegates of this type are used to fetch string representations of user's model objects
+	/// </summary>
+	/// <param name="toRender">The object that is being rendered</param>
+	/// <returns></returns>
+	public delegate string AspectGetterDelegate<T> (T toRender) where T : class;
+
+}
