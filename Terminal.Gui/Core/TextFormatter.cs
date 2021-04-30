dbd36d9d2732a01292a22a794e21c36ac0055978
José Miguel Perricone <jmperricone@hotmail.com>
2021-04-29 22:56:05 -0300

    add direction summaries (#1274)

--- Terminal.Gui/Core/TextFormatter.cs
+++ Terminal.Gui/Core/TextFormatter.cs
@@ -68,35 +68,43 @@ namespace Terminal.Gui {
 	/// </summary>
 	public enum TextDirection {
 		/// <summary>
-		/// Normal Horizontal
+		/// Normal horizontal direction.
+		/// <code>HELLO<br/>WORLD</code>
 		/// </summary>
 		LeftRight_TopBottom,
 		/// <summary>
-		/// Normal Vertical
+		/// Normal vertical direction.
+		/// <code>H W<br/>E O<br/>L R<br/>L L<br/>O D</code>
 		/// </summary>
 		TopBottom_LeftRight,
 		/// <summary>
-		/// 
+		/// This is a horizontal direction. <br/> RTL
+		/// <code>OLLEH<br/>DLROW</code>
 		/// </summary>
 		RightLeft_TopBottom,
 		/// <summary>
-		/// 
+		/// This is a vertical direction.
+		/// <code>W H<br/>O E<br/>R L<br/>L L<br/>D O</code>
 		/// </summary>
 		TopBottom_RightLeft,
 		/// <summary>
-		/// 
+		/// This is a horizontal direction.
+		/// <code>WORLD<br/>HELLO</code>
 		/// </summary>
 		LeftRight_BottomTop,
 		/// <summary>
-		/// 
+		/// This is a vertical direction.
+		/// <code>O D<br/>L L<br/>L R<br/>E O<br/>H W</code>
 		/// </summary>
 		BottomTop_LeftRight,
 		/// <summary>
-		/// 
+		/// This is a horizontal direction.
+		/// <code>DLROW<br/>OLLEH</code>
 		/// </summary>
 		RightLeft_BottomTop,
 		/// <summary>
-		/// 
+		/// This is a vertical direction.
+		/// <code>D O<br/>L L<br/>R L<br/>O E<br/>W H</code>
 		/// </summary>
 		BottomTop_RightLeft
 	}
