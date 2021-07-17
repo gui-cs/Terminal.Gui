c4843c2b4db5975799a71b700b15238f27f84931
Thomas Nind <TZNind@dundee.ac.uk>
2021-07-17 09:53:26 +0100

    Added tests for LineView constructor

---
+++ UnitTests/LineViewTests.cs
@@ -0,0 +1,37 @@
+﻿using Terminal.Gui;
+using Terminal.Gui.Graphs;
+using Terminal.Gui.Views;
+using Xunit;
+
+namespace UnitTests {
+	public class LineViewTests {
+
+		[Fact]
+		public void LineView_DefaultConstructor()
+		{
+			var horizontal = new LineView();
+
+			Assert.Equal (Orientation.Horizontal, horizontal.Orientation);
+			Assert.Equal (Dim.Fill (), horizontal.Width);
+			Assert.Equal (1, horizontal.Height);
+		}
+		[Fact]
+		public void LineView_Horizontal ()
+		{
+			var horizontal = new LineView (Orientation.Horizontal);
+
+			Assert.Equal (Orientation.Horizontal, horizontal.Orientation);
+			Assert.Equal (Dim.Fill (), horizontal.Width);
+			Assert.Equal (1, horizontal.Height);
+		}
+		[Fact]
+		public void LineView_Vertical ()
+		{
+			var vert = new LineView (Orientation.Vertical);
+
+			Assert.Equal (Orientation.Vertical, vert.Orientation);
+			Assert.Equal (Dim.Fill (), vert.Width);
+			Assert.Equal (1, vert.Height);
+		}
+	}
+}
