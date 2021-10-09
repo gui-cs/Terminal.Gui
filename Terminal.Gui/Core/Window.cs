d5f979ae4d92de54fe9cf2f9ad0901f3bd8b5a22
BDisp <bd.bdisp@gmail.com>
2021-10-09 20:02:27 +0100

    Prevents null reference.

--- Terminal.Gui/Core/Window.cs
+++ Terminal.Gui/Core/Window.cs
@@ -40,7 +40,7 @@ namespace Terminal.Gui {
 		public override Border Border {
 			get => base.Border;
 			set {
-				if (base.Border != null && base.Border.Child != null && value.Child == null) {
+				if (base.Border?.Child != null && value?.Child == null) {
 					value.Child = base.Border.Child;
 				}
 				base.Border = value;
