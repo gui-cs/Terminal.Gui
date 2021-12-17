0dd9fae126c92bf0811eb51394c4af3f5b181126
BDisp <bd.bdisp@gmail.com>
2021-12-17 17:01:40 +0000

    Implementing the KeyBinding and KeyBindings with unit tests.

---
+++ Terminal.Gui/Core/KeyBindings.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terminal.Gui {
+	public sealed class KeyBinding {
+		public KeyBinding (View view, Key inKey, Key outKey, string description = "", bool enabled = true)
+		{
+			if (view == null) {
+				throw new ArgumentNullException ("View cannot be null", nameof (view));
+			}
+			View = view.GetType ().Name;
+			InKey = inKey;
+			OutKey = outKey;
+			Description = description;
+			Enabled = enabled;
+		}
+
+		public string View { get; }
+		public Key InKey { get; }
+		public Key OutKey { get; }
+		public string Description { get; }
+		public bool Enabled { get; set; } = true;
+	}
+
+	public sealed class KeyBindings {
+		private class KeyBindingEqualityComparer : IEqualityComparer<KeyBinding> {
+			public bool Equals (KeyBinding x, KeyBinding y)
+			{
+				if (x.View == y.View && (x.InKey == y.InKey || x.OutKey == y.OutKey))
+					return true;
+				return false;
+			}
+
+			public int GetHashCode (KeyBinding obj)
+			{
+				if (obj == null)
+					throw new ArgumentNullException ();
+
+				int hCode = 0;
+				foreach (var c in obj.View) {
+					hCode += c;
+				}
+				return hCode.GetHashCode ();
+			}
+		}
+
+		//private class ViewBindingStatus {
+		//	public ViewBindingStatus (string view, bool enabled)
+		//	{
+		//		View = view;
+		//		Enabled = enabled;
+		//	}
+
+		//	public string View { get; set; }
+		//	public bool Enabled { get; set; } = true;
+		//}
+
+		public KeyBindings ()
+		{
+		}
+
+		public KeyBindings (View view, Key inKey, Key outKey, string description = "", bool enabled = true)
+		{
+			AddKey (view, inKey, outKey, description, enabled);
+		}
+
+		public KeyBindings (KeyBinding keyBinding)
+		{
+			AddKey (keyBinding);
+		}
+
+		public List<(string View, bool Enabled)> Views { get; private set; } = new List<(string View, bool Enabled)> ();
+
+		public List<KeyBinding> Keys { get; private set; } = new List<KeyBinding> ();
+
+		public bool Enabled { get; set; } = false;
+
+		public Key EnableKey { get; set; } = Key.Enter;
+
+		public Key DisableKey { get; set; } = Key.Esc;
+
+		public int Count => Views.Count;
+
+		public bool AddKey (View view, Key inKey, Key outKey, string description = "", bool enabled = true)
+		{
+			if (view == null) {
+				throw new ArgumentNullException ("View cannot be null", nameof (view));
+			}
+			var kb = new KeyBinding (view, inKey, outKey, description, enabled);
+			var viewName = view.GetType ().Name;
+			if (!Views.Any (x => x.View == viewName)) {
+				Views.Add ((viewName, true));
+			}
+			if (Keys.Contains (kb, new KeyBindingEqualityComparer ())) {
+				throw new ArgumentException ("One of the keys already exists.", nameof (view));
+			}
+			Keys.Add (kb);
+			return true;
+		}
+
+		public bool AddKey (KeyBinding keyBinding)
+		{
+			View view = GetInstance (keyBinding.View);
+			return AddKey (view, keyBinding.InKey, keyBinding.OutKey, keyBinding.Description, keyBinding.Enabled);
+		}
+
+		public bool RemoveKey (View view, Key inKey, Key outKey)
+		{
+			var kb = new KeyBinding (view, inKey, outKey);
+			var viewName = view.GetType ().Name;
+			var idx = Keys.FindIndex (x => x.View == viewName && x.InKey == inKey && x.OutKey == outKey);
+			if (idx == -1) {
+				return false;
+			}
+			var count = Keys.Count;
+			Keys.RemoveAt (idx);
+			if (Keys.FirstOrDefault (x => x.View == viewName) == null) {
+				Views.Remove (Views.FirstOrDefault (x => x.View == viewName));
+			}
+			if (count == Keys.Count) {
+				return false;
+			}
+			return true;
+		}
+
+		public bool RemoveKey (string view, Key inKey, Key outKey)
+		{
+			View v = GetInstance (view);
+			return RemoveKey (v, inKey, outKey);
+		}
+
+		public bool RemoveKey (KeyBinding keyBinding)
+		{
+			View view = GetInstance (keyBinding.View);
+			return RemoveKey (view, keyBinding.InKey, keyBinding.OutKey);
+		}
+
+		public bool RemoveAll (View view)
+		{
+			if (view == null) {
+				throw new ArgumentNullException ("View cannot be null", nameof (view));
+			}
+			var viewName = view.GetType ().Name;
+			var result = Keys.RemoveAll (x => x.View == viewName) > 0;
+			if (result) {
+				Views.Remove (Views.FirstOrDefault (x => x.View == viewName));
+			}
+			return result;
+		}
+
+		public bool RemoveAll ()
+		{
+			if (Views.Count > 0) {
+				Views = new List<(string View, bool Enabled)> ();
+				Keys = new List<KeyBinding> ();
+				return true;
+			}
+			return false;
+		}
+
+		public bool EnableDisableKeyBinding (View view, Key inKey, Key outKey, bool toReplace)
+		{
+			if (view == null) {
+				throw new ArgumentNullException ("View cannot be null", nameof (view));
+			}
+			var viewName = view.GetType ().Name;
+			var kb = Keys.FirstOrDefault (x => x.View == viewName && x.InKey == inKey && x.OutKey == outKey);
+			if (kb.Enabled != toReplace) {
+				kb.Enabled = toReplace;
+				return true;
+			}
+			return false;
+		}
+
+		public bool EnableDisableKeyBinding (string view, Key inKey, Key outKey, bool toReplace)
+		{
+			View v = GetInstance (view);
+			return EnableDisableKeyBinding (v, inKey, outKey, toReplace);
+		}
+
+		public bool EnableDisableKeyBinding(KeyBinding keyBinding, bool toReplace)
+		{
+			if (keyBinding == null) {
+				throw new ArgumentNullException ("KeyBinding cannot be null", nameof (keyBinding));
+			}
+			View view = GetInstance (keyBinding.View);
+			return EnableDisableKeyBinding (view, keyBinding.InKey, keyBinding.OutKey, toReplace);
+		}
+
+		private View GetInstance (string strFullyQualifiedName)
+		{
+			foreach (Type type in typeof (View).Assembly.GetTypes ()
+				.Where (myType => myType.IsClass && !myType.IsAbstract && myType.IsPublic && myType.IsSubclassOf (typeof (View)))) {
+				if (type != null && type.Name == strFullyQualifiedName)
+					return (View)Activator.CreateInstance (type);
+			}
+			return null;
+		}
+	}
+
+}
---
+++ UnitTests/KeyBindingsTests.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace Terminal.Gui.Core {
+	public class KeyBindingsTests {
+
+		[Fact]
+		public void KeyBinding_Constructors ()
+		{
+			KeyBinding binding = new KeyBinding (new Window (), (Key)'j', Key.CursorDown);
+			Assert.Equal (typeof (Window).Name, binding.View);
+			Assert.Equal ((Key)'j', binding.InKey);
+			Assert.Equal (Key.CursorDown, binding.OutKey);
+			Assert.Equal ("", binding.Description);
+			Assert.True (binding.Enabled);
+		}
+
+		[Fact]
+		public void KeyBindings_Constructors ()
+		{
+			KeyBindings bindings = new KeyBindings ();
+			Assert.Empty (bindings.Views);
+			Assert.Empty (bindings.Keys);
+			Assert.False (bindings.Enabled);
+			Assert.Equal (Key.Enter, bindings.EnableKey);
+			Assert.Equal (Key.Esc, bindings.DisableKey);
+			Assert.Equal (0, bindings.Count);
+
+			bindings = new KeyBindings (new Window (), (Key)'j', Key.CursorDown);
+			Assert.Equal (typeof (Window).Name, bindings.Views [0].View);
+			Assert.True (bindings.Views [0].Enabled);
+			Assert.Equal (typeof (Window).Name, bindings.Keys [0].View);
+			Assert.False (bindings.Enabled);
+			Assert.Equal (Key.Enter, bindings.EnableKey);
+			Assert.Equal (Key.Esc, bindings.DisableKey);
+			Assert.Equal (1, bindings.Count);
+
+			bindings = new KeyBindings (new KeyBinding (new Window (), (Key)'j', Key.CursorDown));
+			Assert.Equal (typeof (Window).Name, bindings.Views [0].View);
+			Assert.True (bindings.Views [0].Enabled);
+			Assert.Equal (typeof (Window).Name, bindings.Keys [0].View);
+			Assert.False (bindings.Enabled);
+			Assert.Equal (Key.Enter, bindings.EnableKey);
+			Assert.Equal (Key.Esc, bindings.DisableKey);
+			Assert.Equal (1, bindings.Count);
+		}
+
+		[Fact]
+		public void KeyBinding_Allows_Intances ()
+		{
+			var view = new TextView ();
+			KeyBindings bindings = new KeyBindings (view, (Key)'j', Key.CursorDown);
+			Assert.Equal (view.GetType ().Name, bindings.Views [0].View);
+			Assert.True (bindings.Views [0].Enabled);
+			Assert.Equal (view.GetType ().Name, bindings.Keys [0].View);
+			Assert.False (bindings.Enabled);
+			Assert.Equal (Key.Enter, bindings.EnableKey);
+			Assert.Equal (Key.Esc, bindings.DisableKey);
+			Assert.Equal(1, bindings.Count);
+		}
+
+		[Fact]
+		public void Enabled_Property ()
+		{
+			KeyBindings bindings = new KeyBindings (new Window (), (Key)'j', Key.CursorDown);
+			Assert.False (bindings.Enabled);
+
+			bindings.Enabled = true;
+			Assert.True (bindings.Enabled);
+		}
+
+		[Fact]
+		public void AddKey_Methods ()
+		{
+			KeyBindings bindings = new KeyBindings (new TextView (), (Key)'j', Key.CursorDown);
+			Assert.Equal (typeof (TextView).Name, bindings.Views [0].View);
+			Assert.Equal (typeof (TextView).Name, bindings.Keys [0].View);
+
+			bindings.AddKey (new TextField (), (Key)'j', Key.CursorDown);
+			Assert.Equal (typeof (TextField).Name, bindings.Views [1].View);
+			Assert.Equal (typeof (TextField).Name, bindings.Keys [1].View);
+
+			bindings.AddKey (new KeyBinding (new TextField (), (Key)'k', Key.CursorUp));
+			Assert.Equal (typeof (TextField).Name, bindings.Views [1].View);
+			Assert.Equal (typeof (TextField).Name, bindings.Keys [2].View);
+
+			bindings.AddKey (new KeyBinding (new TextField (), Key.CursorUp, (Key)'k'));
+			Assert.Equal (typeof (TextField).Name, bindings.Views [1].View);
+			Assert.Equal (typeof (TextField).Name, bindings.Keys [3].View);
+		}
+
+		[Fact]
+		public void AddKey_Throws_If_View_Is_Null ()
+		{
+			var bindings = new KeyBindings ();
+			Assert.Throws<ArgumentNullException> (() => bindings.AddKey (null, (Key)'j', Key.CursorDown));
+		}
+
+		[Fact]
+		public void AddKey_Throws_On_Existing_Key_In_The_Given_View ()
+		{
+			KeyBindings bindings = new KeyBindings (new TextView (), (Key)'j', Key.CursorDown);
+			Assert.Throws<ArgumentException> (() => bindings.AddKey (new TextView (), (Key)'j', Key.CursorDown));
+			Assert.Throws<ArgumentException> (() => bindings.AddKey (new TextView (), (Key)'k', Key.CursorDown));
+			Assert.Throws<ArgumentException> (() => bindings.AddKey (new TextView (), (Key)'j', Key.CursorUp));
+		}
+
+		[Fact]
+		public void RemoveKey_Methods ()
+		{
+			KeyBindings bindings = new KeyBindings (new TextView (), (Key)'j', Key.CursorDown);
+			Assert.Equal (typeof (TextView).Name, bindings.Views [0].View);
+			Assert.Equal (typeof (TextView).Name, bindings.Keys [0].View);
+
+			bindings.AddKey (new TextField (), (Key)'j', Key.CursorDown);
+			Assert.Equal (typeof (TextField).Name, bindings.Views [1].View);
+			Assert.Equal (typeof (TextField).Name, bindings.Keys [1].View);
+
+			bindings.AddKey (new KeyBinding (new TextField (), (Key)'k', Key.CursorUp));
+			Assert.Equal (typeof (TextField).Name, bindings.Views [1].View);
+			Assert.Equal (typeof (TextField).Name, bindings.Keys [2].View);
+
+			bindings.AddKey (new KeyBinding (new ListView (), (Key)'l', Key.CursorRight));
+			Assert.Equal (typeof (ListView).Name, bindings.Views [2].View);
+			Assert.Equal (typeof (ListView).Name, bindings.Keys [3].View);
+			Assert.Equal (3, bindings.Views.Count);
+			Assert.Equal (4, bindings.Keys.Count);
+
+			Assert.True (bindings.RemoveKey (new TextField (), (Key)'j', Key.CursorDown));
+			Assert.Equal (3, bindings.Views.Count);
+			Assert.Equal (3, bindings.Keys.Count);
+
+			Assert.True (bindings.RemoveKey (new KeyBinding (new TextView (), (Key)'j', Key.CursorDown)));
+			Assert.Equal (2, bindings.Views.Count);
+			Assert.Equal (2, bindings.Keys.Count);
+
+			Assert.True (bindings.RemoveKey (new ListView (), (Key)'l', Key.CursorRight));
+			Assert.Single (bindings.Views);
+			Assert.Single (bindings.Keys);
+
+			Assert.False (bindings.RemoveKey (new TextView (), (Key)'k', Key.CursorUp));
+			Assert.Single (bindings.Views);
+			Assert.Single (bindings.Keys);
+
+			var kb = bindings.Keys [0];
+			Assert.True (bindings.RemoveKey (kb.View, kb.InKey, kb.OutKey));
+			Assert.Empty (bindings.Views);
+			Assert.Empty (bindings.Keys);
+		}
+
+		[Fact]
+		public void RemoveKey_Throws_If_View_Is_Null ()
+		{
+			var bindings = new KeyBindings ();
+			Assert.Throws<ArgumentNullException> (() => bindings.RemoveKey ((View)null, (Key)'j', Key.CursorDown));
+		}
+
+		[Fact]
+		public void RemoveAll_Methods ()
+		{
+			KeyBindings bindings = new KeyBindings (new TextView (), (Key)'j', Key.CursorDown);
+			bindings.AddKey (new TextField (), (Key)'j', Key.CursorDown);
+			bindings.AddKey (new KeyBinding (new TextField (), (Key)'k', Key.CursorUp));
+			Assert.Equal (2, bindings.Views.Count);
+			Assert.Equal (3, bindings.Keys.Count);
+
+			Assert.True (bindings.RemoveAll (new TextView ()));
+			Assert.Single (bindings.Views);
+			Assert.Equal (2, bindings.Keys.Count);
+
+			Assert.True (bindings.RemoveAll ());
+			Assert.Empty (bindings.Views);
+			Assert.Empty (bindings.Keys);
+
+			Assert.False (bindings.RemoveAll ());
+		}
+
+		[Fact]
+		public void RemoveAll_Throws_If_View_Is_Null ()
+		{
+			var bindings = new KeyBindings ();
+			Assert.Throws<ArgumentNullException> (() => bindings.RemoveAll ((View)null));
+		}
+
+		[Fact]
+		public void EnableDisableKeyBinding_Methods ()
+		{
+			KeyBindings bindings = new KeyBindings (new TextView (), (Key)'j', Key.CursorDown);
+			Assert.True (bindings.Keys [0].Enabled);
+
+			bindings.AddKey (new TextField (), (Key)'j', Key.CursorDown);
+			Assert.True (bindings.Keys [1].Enabled);
+
+			bindings.AddKey (new ListView (), (Key)'l', Key.CursorRight);
+			Assert.True (bindings.Keys [2].Enabled);
+
+			bindings.EnableDisableKeyBinding (new TextView (), (Key)'j', Key.CursorDown, false);
+			Assert.False (bindings.Keys [0].Enabled);
+
+			bindings.EnableDisableKeyBinding (new KeyBinding (new TextField (), (Key)'j', Key.CursorDown), false);
+			Assert.False (bindings.Keys [1].Enabled);
+
+			KeyBinding binding = bindings.Keys [2];
+			bindings.EnableDisableKeyBinding (binding.View, binding.InKey, binding.OutKey, false);
+			Assert.False (bindings.Keys [2].Enabled);
+		}
+
+		[Fact]
+		public void EnableDisableKeyBinding_Throws_If_View_Is_Null ()
+		{
+			var bindings = new KeyBindings ();
+			Assert.Throws<ArgumentNullException> (() => bindings.EnableDisableKeyBinding ((View)null, (Key)'j', Key.CursorDown, false));
+			Assert.Throws<ArgumentNullException> (() => bindings.EnableDisableKeyBinding ((string)null, (Key)'j', Key.CursorDown, false));
+			Assert.Throws<ArgumentNullException> (() => bindings.EnableDisableKeyBinding (null, false));
+		}
+
+		[Fact]
+		[AutoInitShutdown]
+		public void KeyPress_Event ()
+		{
+			var kbs = Application.KeyBindings = new KeyBindings (new TextView (), (Key)'j', Key.CursorDown);
+			kbs.Enabled = true;
+
+			var top = Application.Top;
+
+			var tv = new TextView () {
+				Width = 10,
+				Height = 5,
+				Text = "Testing key bindings..."
+			};
+			tv.KeyPress += (e) => {
+				Assert.Equal (Key.CursorDown, e.KeyEvent.Key);
+				e.Handled = true;
+			};
+			top.Add (tv);
+
+			var rs = Application.Begin (top);
+			rs.Toplevel.Running = true;
+
+			Application.Driver.SendKeys ('j', ConsoleKey.J, false, false, false);
+
+		}
+
+
+		[Fact]
+		[AutoInitShutdown]
+		public void ProcessKey_Event ()
+		{
+			var kbs = Application.KeyBindings = new KeyBindings (new TextViewBinding (), (Key)'j', Key.CursorDown);
+			kbs.Enabled = true;
+
+			var top = Application.Top;
+
+			var tv = new TextViewBinding () {
+				Width = 10,
+				Height = 5,
+				Text = "Testing key bindings..."
+			};
+			top.Add (tv);
+
+			var rs = Application.Begin (top);
+			rs.Toplevel.Running = true;
+
+			Application.Driver.SendKeys ('j', ConsoleKey.J, false, false, false);
+
+		}
+
+		private class TextViewBinding : TextView {
+
+			public override bool ProcessKey (KeyEvent kb)
+			{
+				Assert.Equal (Key.CursorDown, kb.Key);
+				return true;
+			}
+		}
+
+	}
+}
