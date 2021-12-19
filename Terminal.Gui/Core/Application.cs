e96f01738738c56c49b700d36ee662fb40a11896
BDisp <bd.bdisp@gmail.com>
2021-12-19 23:10:09 +0000

    Trying fixing the build action.

--- Terminal.Gui/Core/Application.cs
+++ Terminal.Gui/Core/Application.cs
@@ -1203,8 +1203,8 @@ namespace Terminal.Gui {
 			if (MdiTop != null && !Current.Modal) {
 				lock (toplevels) {
 					toplevels.MoveNext ();
-					var isMdi = false;
-					while (toplevels.Peek () == MdiTop || !toplevels.Peek ().Visible) {
+					bool isMdi;
+					while (isMdi = toplevels.Peek () == MdiTop || !toplevels.Peek ().Visible) {
 						if (!isMdi && toplevels.Peek () == MdiTop) {
 							isMdi = true;
 						} else if (isMdi && toplevels.Peek () == MdiTop) {
