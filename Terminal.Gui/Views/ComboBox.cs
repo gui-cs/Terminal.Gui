2aaff2906811ea366466e7b7bda69da0d842db26
BDisp <bd.bdisp@gmail.com>
2022-02-03 18:18:35 +0000

    Fixes some bugs with SelectedItem.

--- Terminal.Gui/Views/ComboBox.cs
+++ Terminal.Gui/Views/ComboBox.cs
@@ -31,7 +31,7 @@ namespace Terminal.Gui {
 
 				// Only need to refresh list if its been added to a container view
 				if (SuperView != null && SuperView.Subviews.Contains (this)) {
-					SelectedItem = 0;
+					SelectedItem = -1;
 					search.Text = "";
 					Search_Changed ("");
 					SetNeedsDisplay ();
@@ -158,9 +158,8 @@ namespace Terminal.Gui {
 			};
 		}
 
-		bool isShow = false;
-
-		private int selectedItem;
+		private bool isShow = false;
+		private int selectedItem = -1;
 
 		/// <summary>
 		/// Gets the index of the currently selected item in the <see cref="Source"/>
@@ -174,9 +173,9 @@ namespace Terminal.Gui {
 
 					selectedItem = value;
 					if (selectedItem != -1) {
-						Text = source.ToList () [selectedItem].ToString ();
+						SetValue (source.ToList () [selectedItem].ToString (), true);
 					} else {
-						Text = "";
+						SetValue ("", true);
 					}
 					OnSelectedChanged ();
 				}
@@ -411,24 +410,26 @@ namespace Terminal.Gui {
 			}
 		}
 
-		private void SetValue (object text)
+		private void SetValue (object text, bool isFromSelectedItem = false)
 		{
 			search.TextChanged -= Search_Changed;
 			this.text = search.Text = text.ToString ();
 			search.CursorPosition = 0;
 			search.TextChanged += Search_Changed;
-			selectedItem = GetSelectedItemFromSource (this.text);
-			OnSelectedChanged ();
+			if (!isFromSelectedItem) {
+				selectedItem = GetSelectedItemFromSource (this.text);
+				OnSelectedChanged ();
+			}
 		}
 
 		private void Selected ()
 		{
 			isShow = false;
 			listview.TabStop = false;
-			HideList ();
 
 			if (listview.Source.Count == 0 || (searchset?.Count ?? 0) == 0) {
 				text = "";
+				HideList ();
 				return;
 			}
 
@@ -437,6 +438,7 @@ namespace Terminal.Gui {
 			Search_Changed (search.Text);
 			OnOpenSelectedItem ();
 			Reset (keepSearchText: true);
+			HideList ();
 		}
 
 		private int GetSelectedItemFromSource (ustring value)
