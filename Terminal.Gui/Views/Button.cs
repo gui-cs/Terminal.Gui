297acadf996f83446c2a591dff7b1a8c075dc0d0
James A Sutherland <j@sutherland.pw>
2022-05-27 08:51:14 -0500

    Update Button.cs
    Fix #1750 - stop erroneously suppressing Update calls on property changes

--- Terminal.Gui/Views/Button.cs
+++ Terminal.Gui/Views/Button.cs
@@ -137,8 +137,7 @@ namespace Terminal.Gui {
 				if (hotKey != hk) {
 					HotKey = hk;
 				}
-				if (IsInitialized)
-					Update ();
+				Update ();
 			}
 		}
 
@@ -150,8 +149,7 @@ namespace Terminal.Gui {
 			get => is_default;
 			set {
 				is_default = value;
-				if (IsInitialized)
-					Update ();
+				Update ();
 			}
 		}
 
