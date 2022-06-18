8042f566c068d4c0a6ebe6bd2fe84b30a7b99eec
BDisp <bd.bdisp@gmail.com>
2022-06-18 02:15:32 +0100

    Fixes requested changes.

--- Terminal.Gui/Views/Checkbox.cs
+++ Terminal.Gui/Views/Checkbox.cs
@@ -114,8 +114,7 @@ namespace Terminal.Gui {
 				break;
 			}
 
-			int w = TextFormatter.Size.Width - (TextFormatter.Text.Contains (HotKeySpecifier)
-				? Math.Max (Rune.ColumnWidth (HotKeySpecifier), 0) : 0);
+			int w = TextFormatter.Size.Width - GetHotKeySpecifierLength ();
 			GetCurrentWidth (out int cWidth);
 			var canSetWidth = SetWidth (w, out int rWidth);
 			if (canSetWidth && (cWidth < rWidth || AutoSize)) {
