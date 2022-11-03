604693c9d2414524a65331482ecc710d1c79b156
BDisp <bd.bdisp@gmail.com>
2022-11-03 16:12:21 +0000

    Fixes button numeration.

--- UICatalog/Scenarios/ButtonsEffect3D.cs
+++ UICatalog/Scenarios/ButtonsEffect3D.cs
@@ -14,7 +14,7 @@ namespace UICatalog.Scenarios {
 				Height = 5,
 			};
 
-			buttonRightBottomEffect3D.Add (new Button ("Button 3") {
+			buttonRightBottomEffect3D.Add (new Button ("Button 1") {
 				X = 2,
 				Y = 1,
 				Border = new Border {
@@ -24,7 +24,7 @@ namespace UICatalog.Scenarios {
 				UseEffect3DAnimation = true
 			});
 
-			buttonRightBottomEffect3D.Add (new Button ("Button 4") {
+			buttonRightBottomEffect3D.Add (new Button ("Button 2") {
 				X = 20,
 				Y = 1,
 				Border = new Border {
@@ -42,7 +42,7 @@ namespace UICatalog.Scenarios {
 				Height = 5,
 			};
 
-			buttonRightTopEffect3D.Add (new Button ("Button 5") {
+			buttonRightTopEffect3D.Add (new Button ("Button 3") {
 				X = 2,
 				Y = 1,
 				Border = new Border {
@@ -53,7 +53,7 @@ namespace UICatalog.Scenarios {
 				UseEffect3DAnimation = true
 			});
 
-			buttonRightTopEffect3D.Add (new Button ("Button 6") {
+			buttonRightTopEffect3D.Add (new Button ("Button 4") {
 				X = 20,
 				Y = 1,
 				Border = new Border {
@@ -72,7 +72,7 @@ namespace UICatalog.Scenarios {
 				Height = 5,
 			};
 
-			buttonLeftTopEffect3D.Add (new Button ("Button 7") {
+			buttonLeftTopEffect3D.Add (new Button ("Button 5") {
 				X = 2,
 				Y = 1,
 				Border = new Border {
@@ -83,7 +83,7 @@ namespace UICatalog.Scenarios {
 				UseEffect3DAnimation = true
 			});
 
-			buttonLeftTopEffect3D.Add (new Button ("Button 8") {
+			buttonLeftTopEffect3D.Add (new Button ("Button 6") {
 				X = 20,
 				Y = 1,
 				Border = new Border {
@@ -102,7 +102,7 @@ namespace UICatalog.Scenarios {
 				Height = 5,
 			};
 
-			buttonLeftBottomEffect3D.Add (new Button ("Button 9") {
+			buttonLeftBottomEffect3D.Add (new Button ("Button 7") {
 				X = 2,
 				Y = 1,
 				Border = new Border {
@@ -113,7 +113,7 @@ namespace UICatalog.Scenarios {
 				UseEffect3DAnimation = true
 			});
 
-			buttonLeftBottomEffect3D.Add (new Button ("Button 0") {
+			buttonLeftBottomEffect3D.Add (new Button ("Button 8") {
 				X = 20,
 				Y = 1,
 				Border = new Border {
