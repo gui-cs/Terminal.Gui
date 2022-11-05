d2bcc10193b97d3f4fea9ca00842f0c01d5b3b65
tznind <tznind@dundee.ac.uk>
2022-11-05 09:33:59 +0000

    Removed TabRowView.PositionCursor as we no longer show cursor

--- Terminal.Gui/Views/TabView.cs
+++ Terminal.Gui/Views/TabView.cs
@@ -466,33 +466,6 @@ namespace Terminal.Gui {
 				Width = Dim.Fill ();
 			}
 
-			/// <summary>
-			/// Positions the cursor at the start of the currently selected tab
-			/// </summary>
-			public override void PositionCursor ()
-			{
-				base.PositionCursor ();
-
-				var selected = host.CalculateViewport (Bounds).FirstOrDefault (t => Equals (host.SelectedTab, t.Tab));
-
-				if (selected == null) {
-					return;
-				}
-
-				int y;
-
-				if (host.Style.TabsOnBottom) {
-					y = 1;
-				} else {
-					y = host.Style.ShowTopLine ? 1 : 0;
-				}
-
-				Move (selected.X, y);
-
-
-
-			}
-
 			public override bool OnEnter (View view)
 			{
 				Driver.SetCursorVisibility (CursorVisibility.Invisible);
