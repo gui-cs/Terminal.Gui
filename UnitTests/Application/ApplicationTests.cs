cc79d24bc791416669f0ce55ae8d2747ce2086c7
Charlie Kindel <tig@users.noreply.github.com>
2023-01-22 09:31:53 -0700

    Revert "colorscheme json converter not needed"
    This reverts commit dc46dd519fecf4bfad30657e523669372210cc09.

--- UnitTests/Application/ApplicationTests.cs
+++ UnitTests/Application/ApplicationTests.cs
@@ -24,6 +24,7 @@ namespace Terminal.Gui.ApplicationTests {
 			Assert.Null (Application.Driver);
 			Assert.Null (Application.Top);
 			Assert.Null (Application.Current);
+			Assert.Throws<ArgumentNullException> (() => Application.HeightAsBuffer == true);
 			Assert.Null (Application.MainLoop);
 			Assert.Null (Application.Iteration);
 			Assert.Null (Application.RootMouseEvent);
