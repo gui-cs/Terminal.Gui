a068933f192f5edf62520166826f383a690006dd
Thomas Nind <31306100+tznind@users.noreply.github.com>
2023-02-24 21:57:58 +0000

    Revert "Merges latest LineCanvas into TileView"

--- Terminal.Gui/Core/Graphs/LineCanvas.cs
+++ Terminal.Gui/Core/Graphs/LineCanvas.cs
@@ -66,7 +66,7 @@ namespace Terminal.Gui.Graphs {
 				for (int x = inArea.X; x < inArea.Width; x++) {
 
 					var intersects = lines
-						.Select (l => l.Intersects (inArea.X + x, inArea.Y + y))
+						.Select (l => l.Intersects (x, y))
 						.Where (i => i != null)
 						.ToArray ();
 
