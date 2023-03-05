4d7e23c77e451534ef36999a674bed492c8af1c7
Tigger Kindel <tig@users.noreply.github.com>
2023-03-05 07:53:49 -0700

    Fixed merge conflicts - readded DimTests.cs

--- UnitTests/Types/DimTests.cs
+++ UnitTests/Types/DimTests.cs
@@ -505,30 +505,6 @@ namespace Terminal.Gui.TypeTests {
 				Assert.Equal (19, v3.Frame.Width); // 198*10%=19 * Percent is related to the super-view if it isn't null otherwise the view width
 				Assert.Equal (19, v3.Frame.Height); // 199*10%=19
 
-				f2.Text = "Frame2";
-				Assert.Equal ("Fill(0)", f2.Width.ToString ());
-				Assert.Equal ("Absolute(5)", f2.Height.ToString ());
-				Assert.Equal (49, f2.Frame.Width); // f2.X = Pos.Right(f1), thus 50-1=49
-				Assert.Equal (5, f2.Frame.Height);
-
-				v1.Text = "Button1";
-				Assert.Equal ("Combine(DimView(Width,FrameView()({X=0,Y=0,Width=49,Height=5}))-Absolute(2))", v1.Width.ToString ());
-				Assert.Equal ("Combine(Fill(0)-Absolute(2))", v1.Height.ToString ());
-				Assert.Equal (47, v1.Frame.Width); // 49-2=47
-				Assert.Equal (89, v1.Frame.Height); // 98-2-7=89
-
-				v2.Text = "Button2";
-				Assert.Equal ("Combine(DimView(Width,FrameView()({X=49,Y=0,Width=49,Height=5}))-Absolute(2))", v2.Width.ToString ());
-				Assert.Equal ("Combine(Fill(0)-Absolute(2))", v2.Height.ToString ());
-				Assert.Equal (47, v2.Frame.Width); // 49-2=47
-				Assert.Equal (89, v2.Frame.Height); // 98-2-7=89
-
-				v3.Text = "Button3";
-				Assert.Equal ("Factor(0.1,False)", v3.Width.ToString ());
-				Assert.Equal ("Factor(0.1,False)", v3.Height.ToString ());
-				Assert.Equal (9, v3.Frame.Width); // 98*10%=9 * Percent is related to the super-view if it isn't null otherwise the view width
-				Assert.Equal (9, v3.Frame.Height); // 99*10%=9
-
 				v4.Text = "Button4";
 				v4.AutoSize = false;
 				Assert.Equal ("Absolute(50)", v4.Width.ToString ());
@@ -542,16 +518,16 @@ namespace Terminal.Gui.TypeTests {
 				Assert.Equal (1, v4.Frame.Height); // 1 because is Dim.DimAbsolute
 
 				v5.Text = "Button5";
-				Assert.Equal ("Combine(DimView(Width,Button()({X=2,Y=7,Width=47,Height=89}))-DimView(Width,Button()({X=0,Y=0,Width=9,Height=9})))", v5.Width.ToString ());
-				Assert.Equal ("Combine(DimView(Height,Button()({X=2,Y=7,Width=47,Height=89}))-DimView(Height,Button()({X=0,Y=0,Width=9,Height=9})))", v5.Height.ToString ());
-				Assert.Equal (38, v5.Frame.Width); // 47-9=38
-				Assert.Equal (80, v5.Frame.Height); // 89-9=80
+				Assert.Equal ("Combine(View(Width,Button()({X=2,Y=7,Width=97,Height=189}))-View(Width,Button()({X=0,Y=0,Width=19,Height=19})))", v5.Width.ToString ());
+				Assert.Equal ("Combine(View(Height,Button()({X=2,Y=7,Width=97,Height=189}))-View(Height,Button()({X=0,Y=0,Width=19,Height=19})))", v5.Height.ToString ());
+				Assert.Equal (78, v5.Frame.Width); // 97-9=78
+				Assert.Equal (170, v5.Frame.Height); // 189-19=170
 
 				v6.Text = "Button6";
 				Assert.Equal ("Factor(0.2,True)", v6.Width.ToString ());
 				Assert.Equal ("Factor(0.2,True)", v6.Height.ToString ());
-				Assert.Equal (9, v6.Frame.Width); // 49*20%=9
-				Assert.Equal (18, v6.Frame.Height); // 98-7*20=18
+				Assert.Equal (19, v6.Frame.Width); // 99*20%=19
+				Assert.Equal (38, v6.Frame.Height); // 198-7*20=18
 			};
 
 			Application.Iteration += () => Application.RequestStop ();
