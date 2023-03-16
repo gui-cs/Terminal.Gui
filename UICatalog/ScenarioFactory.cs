0ea3d8a519484c991472c20d8b590f9c3ef0a7d1
tznind <tznind@dundee.ac.uk>
2023-03-16 08:54:59 +0000

    Add ScenarioFactory

---
+++ UICatalog/ScenarioFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UICatalog.Scenarios;
+
+namespace UICatalog {
+	internal class ScenarioFactory {
+
+		Dictionary<Type, Func<Scenario>> scenarios = new Dictionary<Type, Func<Scenario>>();
+
+		public ScenarioFactory ()
+		{
+			// TODO: Add other scenarios here
+			scenarios.Add (typeof (TableEditor), ()=>new TableEditor ());
+		}
+
+		internal Scenario Create (Type type)
+		{
+			return scenarios [type].Invoke ();
+		}
+
+		internal IEnumerable<Type> GetScenarioTypes ()
+		{
+			return scenarios.Keys;
+		}
+	}
+}
