62a32574f56f2155ce2691d737b489ac447336fd
tznind <tznind@dundee.ac.uk>
2023-03-17 23:22:28 +0000

    Move open/save dialog to their own files

--- Terminal.Gui/Windows/OpenDialog.cs
+++ Terminal.Gui/Windows/OpenDialog.cs
@@ -38,56 +38,6 @@ namespace Terminal.Gui {
 	}
 
 	/// <summary>
-	///  The <see cref="SaveDialog"/> provides an interactive dialog box for users to pick a file to 
-	///  save.
-	/// </summary>
-	/// <remarks>
-	/// <para>
-	///   To use, create an instance of <see cref="SaveDialog"/>, and pass it to
-	///   <see cref="Application.Run(Func{Exception, bool})"/>. This will run the dialog modally,
-	///   and when this returns, the <see cref="FileName"/>property will contain the selected file name or 
-	///   null if the user canceled. 
-	/// </para>
-	/// </remarks>
-	public class SaveDialog : FileDialog2 {
-		/// <summary>
-		/// Initializes a new <see cref="SaveDialog"/>.
-		/// </summary>
-		public SaveDialog () : this (title: string.Empty) { }
-
-		/// <summary>
-		/// Initializes a new <see cref="SaveDialog"/>.
-		/// </summary>
-		/// <param name="title">The title.</param>
-		/// <param name="allowedTypes">The allowed types.</param>
-		public SaveDialog (ustring title, List<AllowedType> allowedTypes = null)
-		{
-			//: base (title, prompt: Strings.fdSave, nameFieldLabel: $"{Strings.fdSaveAs}:", message: message, allowedTypes) { }
-			Title = title;
-			Style.OkButtonText = Strings.fdSave;
-
-			if(allowedTypes != null) {
-				AllowedTypes = allowedTypes;
-			}
-		}
-
-
-		/// <summary>
-		/// Gets the name of the file the user selected for saving, or null
-		/// if the user canceled the <see cref="SaveDialog"/>.
-		/// </summary>
-		/// <value>The name of the file.</value>
-		public ustring FileName {
-			get {
-				if (Canceled)
-					return null;
-
-				return Path;
-			}
-		}
-	}
-
-	/// <summary>
 	/// The <see cref="OpenDialog"/>provides an interactive dialog box for users to select files or directories.
 	/// </summary>
 	/// <remarks>
