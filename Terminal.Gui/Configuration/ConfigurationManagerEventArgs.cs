1a924d3b99537270bc57439dcb0653a777b2c5af
BDisp <bd.bdisp@gmail.com>
2023-03-17 18:18:12 +0000

    Grouping ConfigurationManager into ConfigurationManagerEventArgs.cs file.

--- Terminal.Gui/Configuration/ConfigurationManagerEventArgs.cs
+++ Terminal.Gui/Configuration/ConfigurationManagerEventArgs.cs
@@ -15,4 +15,22 @@ namespace Terminal.Gui.Configuration {
 		{
 		}
 	}
+
+	/// <summary>
+	/// Event arguments for the <see cref="ConfigurationManager.ThemeManager"/> events.
+	/// </summary>
+	public class ThemeManagerEventArgs : EventArgs {
+		/// <summary>
+		/// The name of the new active theme..
+		/// </summary>
+		public string NewTheme { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Initializes a new instance of <see cref="ThemeManagerEventArgs"/>
+		/// </summary>
+		public ThemeManagerEventArgs (string newTheme)
+		{
+			NewTheme = newTheme;
+		}
+	}
 }
