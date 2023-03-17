1ea2e1b29b45ef05b815c7e9aa8fef0056a72b4e
tznind <tznind@dundee.ac.uk>
2023-03-17 22:45:27 +0000

    xmldoc

--- Terminal.Gui/Windows/FileDialog2.cs
+++ Terminal.Gui/Windows/FileDialog2.cs
@@ -34,20 +34,77 @@ namespace Terminal.Gui {
 		/// </summary>
 		public class FileDialog2Style {
 
+			/// <summary>
+			/// Gets or sets the header text displayed in the Filename column of the files table.
+			/// </summary>
 			public string FilenameColumnName { get; set; } = Strings.fdFilename;
-			public string SizeColumnName { get; set; } = Strings.fdSize;
-			public string ModifiedColumnName { get; set; } = Strings.fdModified;
-			public string TypeColumnName { get; set; } = Strings.fdType;
-			public string SearchCaption { get; internal set; } = Strings.fdSearchCaption;
-			public string PathCaption { get; internal set; } = Strings.fdPathCaption;
-			public string WrongFileTypeFeedback { get; internal set; } = Strings.fdWrongFileTypeFeedback;
-			public string DirectoryMustExistFeedback { get; internal set; } = Strings.fdDirectoryMustExistFeedback;
-			public string FileAlreadyExistsFeedback { get; internal set; } = Strings.fdFileAlreadyExistsFeedback;
-			public string FileMustExistFeedback { get; internal set; } = Strings.fdFileMustExistFeedback;
-			public string DirectoryAlreadyExistsFeedback { get; internal set; } = Strings.fdDirectoryAlreadyExistsFeedback;
-			public string FileOrDirectoryMustExistFeedback { get; internal set; } = Strings.fdFileOrDirectoryMustExistFeedback;
 
+			/// <summary>
+			/// Gets or sets the header text displayed in the Size column of the files table.
+			/// </summary>
+			public string SizeColumnName { get; set; } = Strings.fdSize;
+
+			/// <summary>
+			/// Gets or sets the header text displayed in the Modified column of the files table.
+			/// </summary>
+			public string ModifiedColumnName { get; set; } = Strings.fdModified;
+
+			/// <summary>
+			/// Gets or sets the header text displayed in the Type column of the files table.
+			/// </summary>
+			public string TypeColumnName { get; set; } = Strings.fdType;
+
+			/// <summary>
+			/// Gets or sets the text displayed in the 'Search' text box when user has not supplied any input yet.
+			/// </summary>
+			public string SearchCaption { get; internal set; } = Strings.fdSearchCaption;
+
+			/// <summary>
+			/// Gets or sets the text displayed in the 'Path' text box when user has not supplied any input yet.
+			/// </summary>
+			public string PathCaption { get; internal set; } = Strings.fdPathCaption;
+
+			/// <summary>
+			/// Gets or sets the text on the 'Ok' button.  Typically you may want to change this to
+			/// "Open" or "Save" etc.
+			/// </summary>
 			public string OkButtonText { get; set; } = "Ok";
+
+			/// <summary>
+			/// Gets or sets error message when user attempts to select a file type that is not one of <see cref="AllowedTypes"/>
+			/// when <see cref="AllowedTypesIsStrict"/> is true.
+			/// </summary>
+			public string WrongFileTypeFeedback { get; internal set; } = Strings.fdWrongFileTypeFeedback;
+
+			/// <summary>
+			/// Gets or sets error message when user selects a directory that does not exist and
+			/// <see cref="OpenMode"/> is <see cref="OpenMode.Directory"/> and <see cref="MustExist"/> is <see langword="true"/>.
+			/// </summary>
+			public string DirectoryMustExistFeedback { get; internal set; } = Strings.fdDirectoryMustExistFeedback;
+
+			/// <summary>
+			/// Gets or sets error message when user <see cref="OpenMode"/> is <see cref="OpenMode.Directory"/>
+			/// and user enters the name of an existing file (File system cannot have a folder with the same name as a file).
+			/// </summary>
+			public string FileAlreadyExistsFeedback { get; internal set; } = Strings.fdFileAlreadyExistsFeedback;
+
+			/// <summary>
+			/// Gets or sets error message when user selects a file that does not exist and
+			/// <see cref="OpenMode"/> is <see cref="OpenMode.File"/> and <see cref="MustExist"/> is <see langword="true"/>.
+			/// </summary>
+			public string FileMustExistFeedback { get; internal set; } = Strings.fdFileMustExistFeedback;
+
+			/// <summary>
+			/// Gets or sets error message when user <see cref="OpenMode"/> is <see cref="OpenMode.File"/>
+			/// and user enters the name of an existing directory (File system cannot have a folder with the same name as a file).
+			/// </summary>
+			public string DirectoryAlreadyExistsFeedback { get; internal set; } = Strings.fdDirectoryAlreadyExistsFeedback;
+
+			/// <summary>
+			/// Gets or sets error message when user selects a file/dir that does not exist and
+			/// <see cref="OpenMode"/> is <see cref="OpenMode.Mixed"/> and <see cref="MustExist"/> is <see langword="true"/>.
+			/// </summary>
+			public string FileOrDirectoryMustExistFeedback { get; internal set; } = Strings.fdFileOrDirectoryMustExistFeedback;
 
 			/// <summary>
 			/// Gets the style settings for the table of files (in currently selected directory).
