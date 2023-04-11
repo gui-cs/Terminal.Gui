0ed6b350fd483445fe346ecd8a0ea87add7ac170
BDisp <bd.bdisp@gmail.com>
2023-04-11 16:06:56 +0100

    Prevents WordBackward or WordForward throw null exception.

--- Terminal.Gui/Views/TextField.cs
+++ Terminal.Gui/Views/TextField.cs
@@ -1059,6 +1059,7 @@ namespace Terminal.Gui {
 					|| (x > 0 && (char)text [x] == ' ')) {
 
 					var newPosBw = GetModel ().WordBackward (x, 0);
+					if (newPosBw == null) return true;
 					sbw = newPosBw.Value.col;
 				}
 				if (sbw != -1) {
@@ -1066,6 +1067,7 @@ namespace Terminal.Gui {
 					PositionCursor (x);
 				}
 				var newPosFw = GetModel ().WordForward (x, 0);
+				if (newPosFw == null) return true;
 				ClearAllSelection ();
 				if (newPosFw.Value.col != -1 && sbw != -1) {
 					point = newPosFw.Value.col;
