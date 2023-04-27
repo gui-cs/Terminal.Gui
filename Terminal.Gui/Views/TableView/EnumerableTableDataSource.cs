196436af9df86b4ae7cb1f9c9c04cc7041281094
tznind <tznind@dundee.ac.uk>
2023-04-27 02:28:52 +0100

    Add code example to xmldoc

--- Terminal.Gui/Views/TableView/EnumerableTableDataSource.cs
+++ Terminal.Gui/Views/TableView/EnumerableTableDataSource.cs
@@ -20,8 +20,16 @@ namespace Terminal.Gui {
 		/// <remarks>The elements of the <paramref name="data"/> collection are recorded during
 		/// construction (immutable) but the properties of those objects are permitted to
 		/// change.</remarks>
-		/// <param name="data"></param>
-		/// <param name="columnDefinitions"></param>
+		/// <param name="data">The data that you want to present.  The members of this collection
+		/// will be frozen after construction.</param>
+		/// <param name="columnDefinitions">
+		/// Getter methods for each property you want to present in the table. For example:
+		/// <code>
+		/// new () {
+		///    { "Colname1", (t)=>t.SomeField},
+		///    { "Colname2", (t)=>t.SomeOtherField}
+		///}
+		/// </code></param>
 		public EnumerableTableDataSource (IEnumerable<T> data, Dictionary<string, Func<T, object>> columnDefinitions)
 		{
 			this.data = data.ToArray ();
