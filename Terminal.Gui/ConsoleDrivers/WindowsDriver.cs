90132d962eac2d2814f97b14cdac19c7c47366d4
BDisp <bd.bdisp@gmail.com>
2023-05-31 00:10:42 +0100

    Fixes #2688. ReadOnly TextView's broken scrolling after version update.

--- Terminal.Gui/ConsoleDrivers/WindowsDriver.cs
+++ Terminal.Gui/ConsoleDrivers/WindowsDriver.cs
@@ -873,7 +873,7 @@ namespace Terminal.Gui {
 						keyUpHandler (new KeyEvent (map, keyModifiers));
 					}
 				}
-				if (!inputEvent.KeyEvent.bKeyDown && inputEvent.KeyEvent.dwControlKeyState == 0) {
+				if (!inputEvent.KeyEvent.bKeyDown && (inputEvent.KeyEvent.dwControlKeyState == 0 || inputEvent.KeyEvent.dwControlKeyState == WindowsConsole.ControlKeyState.EnhancedKey)) {
 					keyModifiers = null;
 				}
 				break;
@@ -908,6 +908,7 @@ namespace Terminal.Gui {
 				break;
 
 			case WindowsConsole.EventType.Focus:
+				keyModifiers = null;
 				break;
 			}
 		}
