97e3776e7bfe766aad79876df04d0e83792d39cc
BDisp <bd.bdisp@gmail.com>
2023-05-31 13:58:31 +0100

    keyModifiers must be reset after key up was been processed.

--- Terminal.Gui/ConsoleDrivers/WindowsDriver.cs
+++ Terminal.Gui/ConsoleDrivers/WindowsDriver.cs
@@ -873,7 +873,7 @@ namespace Terminal.Gui {
 						keyUpHandler (new KeyEvent (map, keyModifiers));
 					}
 				}
-				if (!inputEvent.KeyEvent.bKeyDown && (inputEvent.KeyEvent.dwControlKeyState == 0 || inputEvent.KeyEvent.dwControlKeyState == WindowsConsole.ControlKeyState.EnhancedKey)) {
+				if (!inputEvent.KeyEvent.bKeyDown) {
 					keyModifiers = null;
 				}
 				break;
