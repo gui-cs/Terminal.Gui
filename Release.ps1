570de8f1100a54b154ced9e197fab36cbec58442
Tigger Kindel <tig@users.noreply.github.com>
2023-06-07 09:51:11 -0600

    Trying to get gitversion to do what I want

--- Release.ps1
+++ Release.ps1
@@ -14,11 +14,11 @@ try {
     Write-Host "Pulling latest from upstream branch $branch"
     git pull upstream $branch
 
-    Write-Host "Tagging release with tag $tag"
-    git tag $tag -a -m $releaseMessage
-
     Write-Host "Creating empty commit with message $releaseMessage"
     git commit --allow-empty -m $releaseMessage
+
+    Write-Host "Tagging release with tag $tag"
+    git tag $tag -a -m $releaseMessage
 
     Write-Host "Pushing changes to upstream"
     git push --atomic upstream $branch $tag
