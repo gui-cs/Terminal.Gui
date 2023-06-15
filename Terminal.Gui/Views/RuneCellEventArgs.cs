5b08f1f0ef85060762b9f85169c93fb7543cbec2
BDisp <bd.bdisp@gmail.com>
2023-06-15 18:57:16 +0100

    Change comment.

--- Terminal.Gui/Views/RuneCellEventArgs.cs
+++ Terminal.Gui/Views/RuneCellEventArgs.cs
@@ -16,7 +16,8 @@ namespace Terminal.Gui {
 		public int Col { get; }
 
 		/// <summary>
-		/// The unwrapped row and col index position in the text.
+		/// The unwrapped row and column index into the text containing the RuneCell. 
+		/// Unwrapped means the text without word wrapping or other visual formatting having been applied.
 		/// </summary>
 		public (int Row, int Col) UnwrappedPosition { get; }
 
