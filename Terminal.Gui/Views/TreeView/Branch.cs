1b415e5d45ae1cbf4526922027c627bcb4662d62
BDisp <bd.bdisp@gmail.com>
2023-12-07 15:09:20 +0000

    Trying to fix TestTreeViewColor again.

--- Terminal.Gui/Views/TreeView/Branch.cs
+++ Terminal.Gui/Views/TreeView/Branch.cs
@@ -196,7 +196,7 @@ namespace Terminal.Gui {
 					// use it
 					modelColor = isSelected ? modelScheme.Focus : modelScheme.Normal;
 				} else {
-					modelColor = Attribute.Default;
+					modelColor = new Attribute (Attribute.Default.Foreground, Attribute.Default.Background);
 				}
 			}
 
