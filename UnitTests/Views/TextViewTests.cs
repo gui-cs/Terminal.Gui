11b51d46e13385e7a7fa487ddd88944d9e98dab2
Tigger Kindel <tig@users.noreply.github.com>
2023-12-11 13:14:00 -0700

    Merged v2_develop

--- UnitTests/Views/TextViewTests.cs
+++ UnitTests/Views/TextViewTests.cs
@@ -2495,41 +2495,39 @@ line.
 				Assert.Equal (CursorVisibility.Invisible, tv.DesiredCursorVisibility);
 			}
 		}
-
-		[Theory]
-		[TextViewTestsAutoInitShutdown]
-		[InlineData (Key.Delete)]
-		[InlineData (Key.DeleteChar)]
-		public void WordWrap_Draw_Typed_Keys_After_Text_Is_Deleted (Key del)
-		{
-			Application.Top.Add (_textView);
-			_textView.Text = "Line 1.\nLine 2.";
-			_textView.WordWrap = true;
-			Application.Begin (Application.Top);
-
-			Assert.True (_textView.WordWrap);
-			TestHelpers.AssertDriverContentsWithFrameAre (@"
-Line 1.
-Line 2.", output);
-
-			Assert.True (_textView.ProcessKey (new KeyEvent (Key.End | Key.ShiftMask, new KeyModifiers () { Shift = true })));
-			Assert.Equal ("Line 1.", _textView.SelectedText);
-
-			Assert.True (_textView.ProcessKey (new KeyEvent (del, new KeyModifiers ())));
-			Application.Refresh ();
-			TestHelpers.AssertDriverContentsWithFrameAre ("Line 2.", output);
-
-			Assert.True (_textView.ProcessKey (new KeyEvent (Key.H, new KeyModifiers ())));
-			Assert.NotEqual (Rect.Empty, _textView._needsDisplayRect);
-			Application.Refresh ();
-			TestHelpers.AssertDriverContentsWithFrameAre (@"
-H      
-Line 2.", output);
-		}
 	}
 
-	// WHY IS THIS FAILING TO BUILD!!!
-	// Error: /home/runner/work/Terminal.Gui/Terminal.Gui/UnitTests/Views/TextViewTests.cs(2503,3): error CS0106: The modifier 'public' is not valid for this item 
+	[Theory]
+	[TextViewTestsAutoInitShutdown]
+	[InlineData (Key.Delete)]
+	[InlineData (Key.DeleteChar)]
+	public void WordWrap_Draw_Typed_Keys_After_Text_Is_Deleted (Key del)
+	{
+		Application.Top.Add (_textView);
+		_textView.Text = "Line 1.\nLine 2.";
+		_textView.WordWrap = true;
+		Application.Begin (Application.Top);
+
+		Assert.True (_textView.WordWrap);
+		TestHelpers.AssertDriverContentsWithFrameAre (@"
+Line 1.
+Line 2.", _output);
+
+		Assert.True (_textView.ProcessKeyDown (new (Key.End | Key.ShiftMask)));
+		Assert.Equal ("Line 1.", _textView.SelectedText);
+
+		Assert.True (_textView.ProcessKeyDown (new (del)));
+		Application.Refresh ();
+		TestHelpers.AssertDriverContentsWithFrameAre ("Line 2.", _output);
+
+		Assert.True (_textView.ProcessKeyDown (new (Key.H)));
+		Assert.NotEqual (Rect.Empty, _textView._needsDisplayRect);
+		Application.Refresh ();
+		TestHelpers.AssertDriverContentsWithFrameAre (@"
+H      
+Line 2.", _output);
+	}
+
 	[Fact]
 	[TextViewTestsAutoInitShutdown]
 	public void DesiredCursorVisibility_Horizontal_Navigation ()
