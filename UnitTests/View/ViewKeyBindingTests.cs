495bc8f1e759fd72dbf59ce6a7497bb2e3317d6c
Tigger Kindel <tig@users.noreply.github.com>
2023-12-14 14:12:13 -0700

    Added more KeyBinding scope tests

--- UnitTests/View/ViewKeyBindingTests.cs
+++ UnitTests/View/ViewKeyBindingTests.cs
@@ -15,4 +15,134 @@ public class ViewKeyBindingTests {
 
 	// tests that test KeyBindingScope.Focus and KeyBindingScope.HotKey (tests for KeyBindingScope.Application are in Application/KeyboardTests.cs)
 
+
+
+	public class ScopedKeyBindingView : View {
+		public bool ApplicationCommand { get; set; }
+		public bool HotKeyCommand { get; set; }
+		public bool FocusedCommand { get; set; }
+
+		public ScopedKeyBindingView ()
+		{
+			AddCommand (Command.Save, () => ApplicationCommand = true);
+			AddCommand (Command.Default, () => HotKeyCommand = true);
+			AddCommand (Command.Left, () => FocusedCommand = true);
+			
+			KeyBindings.Add (ConsoleDriverKey.A, KeyBindingScope.Application, Command.Save);
+			HotKey = ConsoleDriverKey.H;
+			KeyBindings.Add (ConsoleDriverKey.F, KeyBindingScope.Focused, Command.Left);
+		}
+	}
+	
+	[Fact]
+	[AutoInitShutdown]
+	public void Focus_KeyBinding ()
+	{
+		var view = new ScopedKeyBindingView ();
+		var invoked = false;
+		view.InvokingKeyBindings += (s, e) => invoked = true;
+
+		Application.Top.Add (view);
+		Application.Begin (Application.Top);
+
+		Application.OnKeyDown (new (ConsoleDriverKey.A));
+		Assert.True (invoked);
+
+		invoked = false;
+		Application.OnKeyDown (new (ConsoleDriverKey.H));
+		Assert.True (invoked);
+
+		invoked = false;
+		Assert.False (view.HasFocus);
+		Application.OnKeyDown (new (ConsoleDriverKey.F));
+		Assert.False (invoked);
+		Assert.False (view.FocusedCommand);
+
+		invoked = false;
+		view.CanFocus = true;
+		view.SetFocus ();
+		Assert.True (view.HasFocus);
+		Application.OnKeyDown (new (ConsoleDriverKey.F));
+		Assert.True (invoked);
+		
+		Assert.True (view.ApplicationCommand);
+		Assert.True (view.HotKeyCommand);
+		Assert.True (view.FocusedCommand);
+	}
+
+	[Fact]
+	[AutoInitShutdown]
+	public void Focus_KeyBinding_Negative ()
+	{
+		var view = new ScopedKeyBindingView ();
+		var invoked = false;
+		view.InvokingKeyBindings += (s, e) => invoked = true;
+
+		Application.Top.Add (view);
+		Application.Begin (Application.Top);
+
+		Application.OnKeyDown (new (ConsoleDriverKey.Z));
+		Assert.False (invoked);
+		Assert.False (view.ApplicationCommand);
+		Assert.False (view.HotKeyCommand);
+		Assert.False (view.FocusedCommand);
+
+		invoked = false;
+		Assert.False (view.HasFocus);
+		Application.OnKeyDown (new (ConsoleDriverKey.F));
+		Assert.False (invoked);
+		Assert.False (view.ApplicationCommand);
+		Assert.False (view.HotKeyCommand);
+		Assert.False (view.FocusedCommand);
+	}
+
+
+	[Fact]
+	[AutoInitShutdown]
+	public void HotKey_KeyBinding ()
+	{
+		var view = new ScopedKeyBindingView ();
+		var invoked = false;
+		view.InvokingKeyBindings += (s, e) => invoked = true;
+
+		Application.Top.Add (view);
+		Application.Begin (Application.Top);
+
+		invoked = false;
+		Application.OnKeyDown (new (ConsoleDriverKey.H));
+		Assert.True (invoked);
+		Assert.True (view.HotKeyCommand);
+
+		view.HotKey = ConsoleDriverKey.Z;
+		invoked = false;
+		view.HotKeyCommand = false;
+		Application.OnKeyDown (new (ConsoleDriverKey.H)); // old hot key
+		Assert.False (invoked);
+		Assert.False (view.HotKeyCommand);
+
+		Application.OnKeyDown (new (ConsoleDriverKey.Z)); // new hot key
+		Assert.True (invoked);
+		Assert.True (view.HotKeyCommand);
+
+	}
+
+	[Fact]
+	[AutoInitShutdown]
+	public void HotKey_KeyBinding_Negative ()
+	{
+		var view = new ScopedKeyBindingView ();
+		var invoked = false;
+		view.InvokingKeyBindings += (s, e) => invoked = true;
+
+		Application.Top.Add (view);
+		Application.Begin (Application.Top);
+
+		Application.OnKeyDown (new (ConsoleDriverKey.Z));
+		Assert.False (invoked);
+		Assert.False (view.HotKeyCommand);
+
+		invoked = false;
+		Application.OnKeyDown (new (ConsoleDriverKey.F));
+		Assert.False (view.HotKeyCommand);
+	}
 }
