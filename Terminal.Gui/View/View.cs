74afd181481df6f710d73350129fced7a4738eb4
BDisp <bd.bdisp@gmail.com>
2023-12-19 23:34:17 +0000

    Fixes #3064. Pos.View returns PosCombine (#3065)

--- Terminal.Gui/View/View.cs
+++ Terminal.Gui/View/View.cs
@@ -493,7 +493,7 @@ namespace Terminal.Gui {
 		/// <returns></returns>
 		public override string ToString ()
 		{
-			return $"{GetType ().Name}({Id})({Frame})";
+			return $"{GetType ().Name}({Id}){Frame}";
 		}
 
 		/// <inheritdoc/>
