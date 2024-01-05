c2bda7a280b66144c8367dfd02432d1cc8c1754b
Tig Kindel <tig@users.noreply.github.com>
2024-01-05 08:00:56 -0700

    Updated nuget refs

--- Terminal.Gui/View/Layout/SizeChangedEventArgs.cs
+++ Terminal.Gui/View/Layout/SizeChangedEventArgs.cs
@@ -1,31 +1,24 @@
 ﻿using System;
 
-namespace Terminal.Gui {
+namespace Terminal.Gui; 
+
+/// <summary>
+/// Args for events about Size (e.g. Resized)
+/// </summary>
+public class SizeChangedEventArgs : EventArgs {
+	/// <summary>
+	/// Creates a new instance of the <see cref="SizeChangedEventArgs" /> class.
+	/// </summary>
+	/// <param name="size"></param>
+	public SizeChangedEventArgs (Size size) => Size = size;
 
 	/// <summary>
-	/// Args for events about Size (e.g. Resized)
+	/// Gets the size the event describes. This should reflect the new/current size after the event.
 	/// </summary>
-	public class SizeChangedEventArgs : EventArgs {
+	public Size Size { get; }
 
-		/// <summary>
-		/// Creates a new instance of the <see cref="SizeChangedEventArgs"/> class.
-		/// </summary>
-		/// <param name="size"></param>
-		public SizeChangedEventArgs (Size size)
-		{
-			Size = size;
-		}
-
-		/// <summary>
-		/// Gets the size the event describes.  This should
-		/// reflect the new/current size after the event
-		/// resolved.
-		/// </summary>
-		public Size Size { get; }
-
-		/// <summary>
-		/// Set to <see langword="true"/> to cause the resize to be cancelled, if appropriate.
-		/// </summary>
-		public bool Cancel { get; set; }
-	}
+	/// <summary>
+	/// Set to <see langword="true" /> to cause the resize to be cancelled, if appropriate.
+	/// </summary>
+	public bool Cancel { get; set; }
 }
