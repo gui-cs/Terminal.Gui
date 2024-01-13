49c11f491fd21665eda80f0f543b10812a02d696
BDisp <bd.bdisp@gmail.com>
2024-01-13 18:57:52 +0000

    Create TestDateAttribute.

--- UnitTests/Views/DateFieldTests.cs
+++ UnitTests/Views/DateFieldTests.cs
@@ -8,7 +8,7 @@ using Xunit;
 
 namespace Terminal.Gui.ViewsTests {
 	public class DateFieldTests {
-		[Fact]
+		[Fact, TestDate]
 		public void Constructors_Defaults ()
 		{
 			var df = new DateField ();
@@ -22,16 +22,16 @@ namespace Terminal.Gui.ViewsTests {
 			Assert.Equal (date, df.Date);
 			Assert.Equal (1, df.CursorPosition);
 			Assert.Equal (new Rect (0, 0, 12, 1), df.Frame);
-			Assert.Equal ($" {date.ToString (CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern)}", df.Text);
+			Assert.Equal ($" {date.ToString (CultureInfo.InvariantCulture.DateTimeFormat.ShortDatePattern)}", df.Text);
 
 			df = new DateField (1, 2, date);
 			Assert.Equal (date, df.Date);
 			Assert.Equal (1, df.CursorPosition);
 			Assert.Equal (new Rect (1, 2, 12, 1), df.Frame);
-			Assert.Equal ($" {date.ToString (CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern)}", df.Text);
+			Assert.Equal ($" {date.ToString (CultureInfo.InvariantCulture.DateTimeFormat.ShortDatePattern)}", df.Text);
 		}
 
-		[Fact]
+		[Fact, TestDate]
 		public void CursorPosition_Min_Is_Always_One_Max_Is_Always_Max_Format ()
 		{
 			var df = new DateField ();
@@ -42,7 +42,7 @@ namespace Terminal.Gui.ViewsTests {
 			Assert.Equal (10, df.CursorPosition);
 		}
 
-		[Fact]
+		[Fact, TestDate]
 		public void CursorPosition_Min_Is_Always_One_Max_Is_Always_Max_Format_After_Selection ()
 		{
 			var df = new DateField ();
@@ -67,11 +67,9 @@ namespace Terminal.Gui.ViewsTests {
 			Assert.Equal (10, df.CursorPosition);
 		}
 
-		[Fact]
+		[Fact, TestDate]
 		public void KeyBindings_Command ()
 		{
-			CultureInfo cultureBackup = CultureInfo.CurrentCulture;
-			CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 			DateField df = new DateField (DateTime.Parse ("12/12/1971")) {
 				ReadOnly = true
 			};
@@ -111,14 +109,11 @@ namespace Terminal.Gui.ViewsTests {
 			Assert.Equal (2, df.CursorPosition);
 			Assert.True (df.NewKeyDownEvent (new (KeyCode.D | KeyCode.AltMask)));
 			Assert.Equal (" 10/02/1971", df.Text);
-			CultureInfo.CurrentCulture = cultureBackup;
 		}
 
-		[Fact]
+		[Fact, TestDate]
 		public void Typing_With_Selection_Normalize_Format ()
 		{
-			CultureInfo cultureBackup = CultureInfo.CurrentCulture;
-			CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 			DateField df = new DateField (DateTime.Parse ("12/12/1971")) {
 				// Start selection at before the first separator /
 				CursorPosition = 2
@@ -133,14 +128,11 @@ namespace Terminal.Gui.ViewsTests {
 			// The format was normalized and replaced again with /
 			Assert.Equal (" 12/12/1971", df.Text);
 			Assert.Equal (4, df.CursorPosition);
-			CultureInfo.CurrentCulture = cultureBackup;
 		}
 
-		[Fact, AutoInitShutdown]
+		[Fact, TestDate, AutoInitShutdown]
 		public void Copy_Paste ()
 		{
-			CultureInfo cultureBackup = CultureInfo.CurrentCulture;
-			CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 			DateField df1 = new DateField (DateTime.Parse ("12/12/1971"));
 			DateField df2 = new DateField (DateTime.Parse ("12/31/2023"));
 			// Select all text
@@ -154,19 +146,15 @@ namespace Terminal.Gui.ViewsTests {
 			Assert.True (df1.NewKeyDownEvent (new (KeyCode.V | KeyCode.CtrlMask)));
 			Assert.Equal (" 12/31/2023", df1.Text);
 			Assert.Equal (11, df1.CursorPosition);
-			CultureInfo.CurrentCulture = cultureBackup;
 		}
 
-		[Fact]
+		[Fact, TestDate]
 		public void Date_Start_From_01_01_0001_And_End_At_12_31_9999 ()
 		{
-			CultureInfo cultureBackup = CultureInfo.CurrentCulture;
-			CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 			DateField df = new DateField (DateTime.Parse ("01/01/0001"));
 			Assert.Equal (" 01/01/0001", df.Text);
 			df.Date = DateTime.Parse ("12/31/9999");
 			Assert.Equal (" 12/31/9999", df.Text);
-			CultureInfo.CurrentCulture = cultureBackup;
 		}
 
 		[Fact]
