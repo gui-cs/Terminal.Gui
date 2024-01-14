1a16c76f29264a04781d181876861d92740bee66
Tig Kindel <tig@users.noreply.github.com>
2024-01-14 16:42:03 -0700

    Moved ColorScheme to ColorScheme.cs

---
+++ UnitTests/Drawing/ColorSchemeTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Terminal.Gui.DrawingTests;
+
+public class ColorSchemeTests {
+	[Fact]
+	public void Colors_ColorSchemes_Property_Has_Private_Setter ()
+	{
+		// Resharper Code Cleanup likes to remove the `private set; `
+		// from the ColorSchemes property.  This test will fail if
+		// that happens.
+		var property = typeof (Colors).GetProperty ("ColorSchemes");
+		Assert.NotNull (property);
+		Assert.NotNull (property.SetMethod);
+		Assert.True (property.GetSetMethod (true).IsPrivate);
+
+	}
+
+	[Fact, AutoInitShutdown]
+	public void ColorScheme_New ()
+	{
+		var scheme = new ColorScheme ();
+		var lbl = new Label ();
+		lbl.ColorScheme = scheme;
+		lbl.Draw ();
+	}
+	
+}
