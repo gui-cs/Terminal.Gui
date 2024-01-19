82219cc80dbabb34c7697534dc3eeda4d29a1c11
Brandon Thetford <github@snapsinazfs.com>
2024-01-19 14:06:57 -0700

    Make this one better

--- UnitTests/Drawing/ColorTests.Constructors.cs
+++ UnitTests/Drawing/ColorTests.Constructors.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Xunit;
 namespace Terminal.Gui.DrawingTests;
 
@@ -83,17 +83,41 @@ public partial class ColorTests {
 		);
 	}
 
-	[Fact]
-	public void Color_Constructor_WithColorName ( )
+	[Theory]
+    [MemberData(nameof(ColorTestsTheoryDataGenerators.Constructor_WithColorName_AllChannelsCorrect), MemberType = typeof(ColorTestsTheoryDataGenerators))]
+	public void Constructor_WithColorName_AllChannelsCorrect (ColorName cname, Color expectedColor, ValueTuple<byte,byte,byte> expectedColorValues )
 	{
-		// Arrange
-		var colorName = ColorName.Blue;
-		var expectedColor = new Color ( 0, 55, 218 ); // Blue
+		var color = new Color ( cname );
 
-		// Act
-		var color = new Color ( colorName );
+		Assert.Multiple (
+			( ) => Assert.Equal (expectedColorValues.Item3, color.B),
+			( ) => Assert.Equal (expectedColorValues.Item2, color.G),
+			( ) => Assert.Equal (expectedColorValues.Item1, color.R),
+			( ) => Assert.Equal (255, color.A)
+		);
+	}
+}
 
-		// Assert
-		Assert.Equal ( expectedColor, color );
+public static partial class ColorTestsTheoryDataGenerators {
+	public static TheoryData<ColorName, Color, ValueTuple<byte,byte,byte>> Constructor_WithColorName_AllChannelsCorrect ( )
+	{
+		TheoryData<ColorName, Color, ValueTuple<byte,byte,byte>> data = [];
+		data.Add ( ColorName.Black, new ( 12, 12, 12 ), new ( 12, 12, 12 ) );
+		data.Add ( ColorName.Blue, new ( 0, 55, 218 ), new ( 0, 55, 218 ) );
+		data.Add ( ColorName.Green, new ( 19, 161, 14 ), new ( 19, 161, 14 ) );
+		data.Add ( ColorName.Cyan, new ( 58, 150, 221 ), new ( 58, 150, 221 ) );
+		data.Add ( ColorName.Red, new ( 197, 15, 31 ), new ( 197, 15, 31 ) );
+		data.Add ( ColorName.Magenta, new ( 136, 23, 152 ), new ( 136, 23, 152 ) );
+		data.Add ( ColorName.Yellow, new ( 128, 64, 32 ), new ( 128, 64, 32 ) );
+		data.Add ( ColorName.Gray, new ( 204, 204, 204 ), new ( 204, 204, 204 ) );
+		data.Add ( ColorName.DarkGray, new ( 118, 118, 118 ), new ( 118, 118, 118 ) );
+		data.Add ( ColorName.BrightBlue, new ( 59, 120, 255 ), new ( 59, 120, 255 ) );
+		data.Add ( ColorName.BrightGreen, new ( 22, 198, 12 ), new ( 22, 198, 12 ) );
+		data.Add ( ColorName.BrightCyan, new ( 97, 214, 214 ), new ( 97, 214, 214 ) );
+		data.Add ( ColorName.BrightRed, new ( 231, 72, 86 ), new ( 231, 72, 86 ) );
+		data.Add ( ColorName.BrightMagenta, new ( 180, 0, 158 ), new ( 180, 0, 158 ) );
+		data.Add ( ColorName.BrightYellow, new ( 249, 241, 165 ), new ( 249, 241, 165 ) );
+		data.Add ( ColorName.White, new ( 242, 242, 242 ), new ( 242, 242, 242 ) );
+		return data;
 	}
 }
