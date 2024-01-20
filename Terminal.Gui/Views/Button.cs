712018eee6808d82c2cd73e58b908f93552e3c94
BDisp <bd.bdisp@gmail.com>
2024-01-20 14:27:47 +0000

    Update xml doc.

--- Terminal.Gui/Views/Button.cs
+++ Terminal.Gui/Views/Button.cs
@@ -129,6 +129,10 @@ namespace Terminal.Gui {
 		/// Gets or sets whether the <see cref="Button"/> is the default action to activate in a dialog.
 		/// </summary>
 		/// <value><c>true</c> if is default; otherwise, <c>false</c>.</value>
+		/// <remarks>
+		/// If is <see langword="true"/> the current focused view
+		/// will remain focused if the window is not closed.
+		/// </remarks>
 		public bool IsDefault {
 			get => is_default;
 			set {
