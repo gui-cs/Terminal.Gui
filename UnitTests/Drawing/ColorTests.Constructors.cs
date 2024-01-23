71bae0bcc1ad41c9d5dcf1b5217ed3381140d5af
Brandon Thetford <github@snapsinazfs.com>
2024-01-23 01:29:52 -0700

    Cleanup
     - Renamed some tests
     - Make a test even clearer
     - Removed redundant code
     - Got rid of unused parameter in Constructor_WithColorName_AllChannelsCorrect

--- UnitTests/Drawing/ColorTests.Constructors.cs
+++ UnitTests/Drawing/ColorTests.Constructors.cs
@@ -1,9 +1,29 @@
-﻿namespace Terminal.Gui.DrawingTests;
+namespace Terminal.Gui.DrawingTests;
 
 public partial class ColorTests {
+
 	[Theory]
-    [CombinatorialData]
-	public void Constructor_WithRGBValues_AllValuesCorrect ( [CombinatorialValues(0,1,254)]byte r, [CombinatorialValues(0,1,253)]byte g, [CombinatorialValues(0,1,252)]byte b )
+	[CombinatorialData]
+	public void Constructor_WithByteRGBAValues_AllValuesCorrect ( [CombinatorialValues ( 0, 1, 254 )] byte r, [CombinatorialValues ( 0, 1, 253 )] byte g, [CombinatorialValues ( 0, 1, 252 )] byte b, [CombinatorialValues ( 0, 1, 251 )] byte a )
+	{
+		var color = new Color ( r, g, b, a );
+
+		ReadOnlySpan<byte> bytes = [b, g, r, a];
+		int expectedRgba = BitConverter.ToInt32 ( bytes );
+		uint expectedArgb = BitConverter.ToUInt32 ( bytes );
+
+		Assert.Multiple (
+			( ) => Assert.Equal ( r, color.R ),
+			( ) => Assert.Equal ( g, color.G ),
+			( ) => Assert.Equal ( b, color.B ),
+			( ) => Assert.Equal ( a, color.A ),
+			( ) => Assert.Equal ( expectedRgba, color.Rgba ),
+			( ) => Assert.Equal ( expectedArgb, color.Argb )
+		);
+	}
+	[Theory]
+	[CombinatorialData]
+	public void Constructor_WithByteRGBValues_AllValuesCorrect ( [CombinatorialValues ( 0, 1, 254 )] byte r, [CombinatorialValues ( 0, 1, 253 )] byte g, [CombinatorialValues ( 0, 1, 252 )] byte b )
 	{
 		var color = new Color ( r, g, b );
 
@@ -22,11 +42,12 @@ public partial class ColorTests {
 	}
 
 	[Theory]
-	[CombinatorialData]
-	public void Constructor_WithInt32RGBValues_AllValuesCorrect ( [CombinatorialRandomData ( Count = 4, Minimum = 0, Maximum = 255 )] int r, [CombinatorialRandomData ( Count = 4, Minimum = 0, Maximum = 255 )] int g, [CombinatorialRandomData ( Count = 4, Minimum = 0, Maximum = 255 )] int b )
+	[MemberData ( nameof ( ColorTestsTheoryDataGenerators.Constructor_WithColorName_AllChannelsCorrect ), MemberType = typeof ( ColorTestsTheoryDataGenerators ) )]
+	public void Constructor_WithColorName_AllChannelsCorrect ( ColorName cname, ValueTuple<byte, byte, byte> expectedColorValues )
 	{
-		var color = new Color ( r, g, b );
+		var color = new Color ( cname );
 
+		( byte r, byte g, byte b ) = expectedColorValues;
 		Assert.Multiple (
 			( ) => Assert.Equal ( r, color.R ),
 			( ) => Assert.Equal ( g, color.G ),
@@ -37,27 +58,7 @@ public partial class ColorTests {
 
 	[Theory]
 	[CombinatorialData]
-	public void Constructor_WithRGBAValues_AllValuesCorrect ( [CombinatorialValues ( 0, 1, 254 )] byte r, [CombinatorialValues ( 0, 1, 253 )] byte g, [CombinatorialValues ( 0, 1, 252 )] byte b, [CombinatorialValues ( 0, 1, 251 )] byte a )
-	{
-		var color = new Color ( r, g, b, a );
-
-		ReadOnlySpan<byte> bytes = [b, g, r, a];
-		int expectedRgba = BitConverter.ToInt32 ( bytes );
-		uint expectedArgb = BitConverter.ToUInt32 ( bytes );
-
-		Assert.Multiple (
-			( ) => Assert.Equal ( r, color.R ),
-			( ) => Assert.Equal ( g, color.G ),
-			( ) => Assert.Equal ( b, color.B ),
-			( ) => Assert.Equal ( a, color.A ),
-			( ) => Assert.Equal ( expectedRgba, color.Rgba ),
-			( ) => Assert.Equal ( expectedArgb, color.Argb )
-		);
-	}
-
-	[Theory]
-	[CombinatorialData]
-	public void Constructor_WithSignedInteger_AllValuesCorrect ([CombinatorialValues ( 0, 1, 254 )] byte r, [CombinatorialValues ( 0, 1, 253 )] byte g, [CombinatorialValues ( 0, 1, 252 )] byte b, [CombinatorialValues ( 0, 1, 251 )] byte a )
+	public void Constructor_WithInt32_AllValuesCorrect ( [CombinatorialValues ( 0, 1, 254 )] byte r, [CombinatorialValues ( 0, 1, 253 )] byte g, [CombinatorialValues ( 0, 1, 252 )] byte b, [CombinatorialValues ( 0, 1, 251 )] byte a )
 	{
 		ReadOnlySpan<byte> bytes = [b, g, r, a];
 		int expectedRgba = BitConverter.ToInt32 ( bytes );
@@ -77,10 +78,37 @@ public partial class ColorTests {
 
 	[Theory]
 	[CombinatorialData]
-	public void Constructor_WithUnsignedInteger_AllChannelsCorrect ([CombinatorialValues ( 0, 1, 254 )] byte r, [CombinatorialValues ( 0, 1, 253 )] byte g, [CombinatorialValues ( 0, 1, 252 )] byte b, [CombinatorialValues ( 0, 1, 251 )] byte a )
+	public void Constructor_WithInt32RGBAValues_AllValuesCorrect ( [CombinatorialRandomData ( Count = 4, Minimum = 0, Maximum = 255 )] int r, [CombinatorialRandomData ( Count = 4, Minimum = 0, Maximum = 255 )] int g, [CombinatorialRandomData ( Count = 4, Minimum = 0, Maximum = 255 )] int b, [CombinatorialRandomData ( Count = 4, Minimum = 0, Maximum = 255 )] int a )
+	{
+		var color = new Color ( r, g, b, a );
+
+		Assert.Multiple (
+			( ) => Assert.Equal ( r, color.R ),
+			( ) => Assert.Equal ( g, color.G ),
+			( ) => Assert.Equal ( b, color.B ),
+			( ) => Assert.Equal ( a, color.A )
+		);
+	}
+
+	[Theory]
+	[CombinatorialData]
+	public void Constructor_WithInt32RGBValues_AllValuesCorrect ( [CombinatorialRandomData ( Count = 4, Minimum = 0, Maximum = 255 )] int r, [CombinatorialRandomData ( Count = 4, Minimum = 0, Maximum = 255 )] int g, [CombinatorialRandomData ( Count = 4, Minimum = 0, Maximum = 255 )] int b )
+	{
+		var color = new Color ( r, g, b );
+
+		Assert.Multiple (
+			( ) => Assert.Equal ( r, color.R ),
+			( ) => Assert.Equal ( g, color.G ),
+			( ) => Assert.Equal ( b, color.B ),
+			( ) => Assert.Equal ( byte.MaxValue, color.A )
+		);
+	}
+
+	[Theory]
+	[CombinatorialData]
+	public void Constructor_WithUInt32_AllChannelsCorrect ( [CombinatorialValues ( 0, 1, 254 )] byte r, [CombinatorialValues ( 0, 1, 253 )] byte g, [CombinatorialValues ( 0, 1, 252 )] byte b, [CombinatorialValues ( 0, 1, 251 )] byte a )
 	{
 		ReadOnlySpan<byte> bytes = [b, g, r, a];
-		int expectedRgba = BitConverter.ToInt32 ( bytes );
 		uint expectedArgb = BitConverter.ToUInt32 ( bytes );
 
 		var color = new Color ( expectedArgb );
@@ -92,42 +120,27 @@ public partial class ColorTests {
 			( ) => Assert.Equal ( a, color.A )
 		);
 	}
-
-	[Theory]
-    [MemberData(nameof(ColorTestsTheoryDataGenerators.Constructor_WithColorName_AllChannelsCorrect), MemberType = typeof(ColorTestsTheoryDataGenerators))]
-	public void Constructor_WithColorName_AllChannelsCorrect (ColorName cname, Color expectedColor, ValueTuple<byte,byte,byte> expectedColorValues )
-	{
-		var color = new Color ( cname );
-
-		Assert.Multiple (
-			( ) => Assert.Equal (expectedColorValues.Item3, color.B),
-			( ) => Assert.Equal (expectedColorValues.Item2, color.G),
-			( ) => Assert.Equal (expectedColorValues.Item1, color.R),
-			( ) => Assert.Equal (255, color.A)
-		);
-	}
 }
-
 public static partial class ColorTestsTheoryDataGenerators {
-	public static TheoryData<ColorName, Color, ValueTuple<byte,byte,byte>> Constructor_WithColorName_AllChannelsCorrect ( )
+	public static TheoryData<ColorName, ValueTuple<byte, byte, byte>> Constructor_WithColorName_AllChannelsCorrect ( )
 	{
-		TheoryData<ColorName, Color, ValueTuple<byte,byte,byte>> data = [];
-		data.Add ( ColorName.Black, new ( 12, 12, 12 ), new ( 12, 12, 12 ) );
-		data.Add ( ColorName.Blue, new ( 0, 55, 218 ), new ( 0, 55, 218 ) );
-		data.Add ( ColorName.Green, new ( 19, 161, 14 ), new ( 19, 161, 14 ) );
-		data.Add ( ColorName.Cyan, new ( 58, 150, 221 ), new ( 58, 150, 221 ) );
-		data.Add ( ColorName.Red, new ( 197, 15, 31 ), new ( 197, 15, 31 ) );
-		data.Add ( ColorName.Magenta, new ( 136, 23, 152 ), new ( 136, 23, 152 ) );
-		data.Add ( ColorName.Yellow, new ( 128, 64, 32 ), new ( 128, 64, 32 ) );
-		data.Add ( ColorName.Gray, new ( 204, 204, 204 ), new ( 204, 204, 204 ) );
-		data.Add ( ColorName.DarkGray, new ( 118, 118, 118 ), new ( 118, 118, 118 ) );
-		data.Add ( ColorName.BrightBlue, new ( 59, 120, 255 ), new ( 59, 120, 255 ) );
-		data.Add ( ColorName.BrightGreen, new ( 22, 198, 12 ), new ( 22, 198, 12 ) );
-		data.Add ( ColorName.BrightCyan, new ( 97, 214, 214 ), new ( 97, 214, 214 ) );
-		data.Add ( ColorName.BrightRed, new ( 231, 72, 86 ), new ( 231, 72, 86 ) );
-		data.Add ( ColorName.BrightMagenta, new ( 180, 0, 158 ), new ( 180, 0, 158 ) );
-		data.Add ( ColorName.BrightYellow, new ( 249, 241, 165 ), new ( 249, 241, 165 ) );
-		data.Add ( ColorName.White, new ( 242, 242, 242 ), new ( 242, 242, 242 ) );
+		TheoryData<ColorName, ValueTuple<byte, byte, byte>> data = [];
+		data.Add ( ColorName.Black, new ValueTuple<byte, byte, byte> ( 12, 12, 12 ) );
+		data.Add ( ColorName.Blue, new ValueTuple<byte, byte, byte> ( 0, 55, 218 ) );
+		data.Add ( ColorName.Green, new ValueTuple<byte, byte, byte> ( 19, 161, 14 ) );
+		data.Add ( ColorName.Cyan, new ValueTuple<byte, byte, byte> ( 58, 150, 221 ) );
+		data.Add ( ColorName.Red, new ValueTuple<byte, byte, byte> ( 197, 15, 31 ) );
+		data.Add ( ColorName.Magenta, new ValueTuple<byte, byte, byte> ( 136, 23, 152 ) );
+		data.Add ( ColorName.Yellow, new ValueTuple<byte, byte, byte> ( 128, 64, 32 ) );
+		data.Add ( ColorName.Gray, new ValueTuple<byte, byte, byte> ( 204, 204, 204 ) );
+		data.Add ( ColorName.DarkGray, new ValueTuple<byte, byte, byte> ( 118, 118, 118 ) );
+		data.Add ( ColorName.BrightBlue, new ValueTuple<byte, byte, byte> ( 59, 120, 255 ) );
+		data.Add ( ColorName.BrightGreen, new ValueTuple<byte, byte, byte> ( 22, 198, 12 ) );
+		data.Add ( ColorName.BrightCyan, new ValueTuple<byte, byte, byte> ( 97, 214, 214 ) );
+		data.Add ( ColorName.BrightRed, new ValueTuple<byte, byte, byte> ( 231, 72, 86 ) );
+		data.Add ( ColorName.BrightMagenta, new ValueTuple<byte, byte, byte> ( 180, 0, 158 ) );
+		data.Add ( ColorName.BrightYellow, new ValueTuple<byte, byte, byte> ( 249, 241, 165 ) );
+		data.Add ( ColorName.White, new ValueTuple<byte, byte, byte> ( 242, 242, 242 ) );
 		return data;
 	}
 }
