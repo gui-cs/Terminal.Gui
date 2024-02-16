d8a9f0f9a0d822f603494e658438a190e6eb2ae9
Brandon Thetford <github@snapsinazfs.com>
2024-02-15 18:54:56 -0700

    Mark this ignored just in case

--- Terminal.Gui/Drawing/Attribute.cs
+++ Terminal.Gui/Drawing/Attribute.cs
@@ -14,6 +14,7 @@ namespace Terminal.Gui;
 public readonly record struct Attribute : IEqualityOperators<Attribute, Attribute, bool>
 {
     /// <summary>Default empty attribute.</summary>
+    [JsonIgnore]
     public static Attribute Default => new (Color.White, ColorName.Black);
 
     /// <summary>The <see cref="ConsoleDriver"/>-specific color value.</summary>
