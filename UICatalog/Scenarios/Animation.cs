2b25aca129cddbdab0cfabdd13dadc49aed369df
Brandon Thetford <github@snapsinazfs.com>
2024-02-20 23:35:51 -0700

    Remove now-invalid usings.

--- UICatalog/Scenarios/Animation.cs
+++ UICatalog/Scenarios/Animation.cs
@@ -7,7 +7,6 @@ using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using Terminal.Gui;
-using Rectangle = Terminal.Gui.Rectangle;
 
 namespace UICatalog.Scenarios;
 
