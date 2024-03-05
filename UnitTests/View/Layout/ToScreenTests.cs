c0ee7d93099f0efdc582764511dfda7663167def
Tig Kindel <tig@users.noreply.github.com>
2024-03-05 16:16:32 -0700

    Adding more unit tests

---
+++ UnitTests/View/Layout/ToScreenTests.cs
@@ -0,0 +1,9 @@
+﻿using Xunit.Abstractions;
+
+public class ToScreenTests (ITestOutputHelper output)
+{
+    private readonly ITestOutputHelper _output = output;
+
+    // Test View.FrameToScreen without Adornments
+
+}
