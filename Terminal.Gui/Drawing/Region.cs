0db3e02769942684352963d0c70fca11054cb5b9
BDisp <bd.bdisp@gmail.com>
2024-04-21 00:17:05 +0100

    Add Region class and unit tests.

---
+++ Terminal.Gui/Drawing/Region.cs
@@ -0,0 +1,239 @@
+﻿#nullable enable
+namespace Terminal.Gui;
+
+/// <summary>
+/// Describes the interior of a screen shape composed of rectangles and paths. This class cannot be inherited.
+/// </summary>
+public sealed class Region : IDisposable
+{
+    private dynamic _rect;
+
+    /// <summary>
+    /// Initializes a new <see cref="Region"/>.
+    /// </summary>
+    public Region () : this (Rectangle.Empty) { }
+
+    /// <summary>
+    /// Initializes a new <see cref="Region"/> from the specified <see cref="Rectangle"/> structure.
+    /// </summary>
+    /// <param name="rect"></param>
+    public Region (Rectangle rect)
+    {
+        _rect = rect;
+    }
+
+    /// <summary>
+    /// Initializes a new <see cref="Region"/> from the specified <see cref="RectangleF"/> structure.
+    /// </summary>
+    /// <param name="rect"></param>
+    public Region (RectangleF rect)
+    {
+        _rect = rect;
+    }
+
+    /// <inheritdoc />
+    public void Dispose ()
+    {
+        throw new NotImplementedException ();
+    }
+
+    /// <inheritdoc />
+    public override bool Equals (object? obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+
+        dynamic thisRect;
+        dynamic otherRect;
+
+        if (_rect.GetType ().Name == "Rectangle")
+        {
+            thisRect = (Rectangle)_rect;
+            otherRect = (Rectangle)((Region)obj)._rect;
+        }
+        else
+        {
+            thisRect = (RectangleF)_rect;
+            otherRect = (RectangleF)((Region)obj)._rect;
+        }
+
+        return thisRect == otherRect;
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode () { return HashCode.Combine (_rect); }
+
+    #region Rectangle, RectangleF
+
+    /// <summary>
+    /// Calculates from the <see cref="Region"/> array to the union and the specified
+    /// <see cref="Rectangle"/> or <see cref="RectangleF"/> structure of each other.
+    /// </summary>
+    /// <param name="regions"></param>
+    /// <returns></returns>
+    public static dynamic Union (params Region [] regions)
+    {
+        dynamic? rBase = null;
+
+        foreach (Region r in regions)
+        {
+            if (rBase is null)
+            {
+                rBase = r._rect;
+                continue;
+            }
+            if (rBase.GetType ().Name == "Rectangle")
+            {
+                Rectangle rRect = r._rect.GetType ().Name == "RectangleF" ? Rectangle.Round (r._rect) : r._rect;
+                rBase = Rectangle.Union (rBase, rRect);
+            }
+            else
+            {
+                RectangleF rRect = r._rect;
+                rBase = RectangleF.Union (rBase, rRect);
+            }
+        }
+
+        if (rBase is { })
+        {
+            return rBase;
+        }
+
+        if (rBase.GetType ().Name == "Rectangle")
+        {
+            return Rectangle.Empty;
+        }
+
+        return RectangleF.Empty;
+    }
+
+    /// <summary>
+    /// Calculates from the <see cref="Region"/> <see cref="HashSet{T}"/> to the union and the specified
+    /// <see cref="Rectangle"/> or <see cref="RectangleF"/> structure of each other.
+    /// </summary>
+    /// <param name="regions"></param>
+    /// <returns></returns>
+    public static dynamic Union (HashSet<Region> regions)
+    {
+        return Union (regions.ToArray ());
+    }
+
+    /// <summary>
+    /// Updates this <see cref="Region"/> to the union of itself and the specified
+    /// <see cref="Rectangle"/> or <see cref="RectangleF"/> structure.
+    /// </summary>
+    /// <param name="rect"></param>
+    /// <returns></returns>
+    public dynamic Union (dynamic rect)
+    {
+        _rect = rect.GetType ().Name == "Rectangle" ? Rectangle.Union ((Rectangle)_rect, rect) : RectangleF.Union ((RectangleF)_rect, rect);
+
+        return _rect;
+    }
+
+    /// <summary>
+    /// Updates this <see cref="Region"/> to the union of itself and the specified <see cref="Region"/>.
+    /// </summary>
+    /// <param name="region"></param>
+    /// <returns></returns>
+    public dynamic Union (Region region)
+    {
+        return Union (region._rect);
+    }
+
+    /// <summary>
+    /// Gets a <see cref="Rectangle"/> structure that represents a rectangle that bounds this <see cref="Region"/> on the drawing surface of a <see cref="View"/> array.
+    /// </summary>
+    /// <param name="views"></param>
+    /// <returns></returns>
+    public static Rectangle GetViewsBounds (params View [] views)
+    {
+        return Union (views.Select (c => new Region (c.Viewport)).ToArray ());
+    }
+
+    /// <summary>
+    /// Calculates from the <see cref="Region"/> array to the intersection and the specified
+    /// <see cref="Rectangle"/> or <see cref="RectangleF"/> structure of each other.
+    /// </summary>
+    /// <param name="regions"></param>
+    /// <param name="rect"></param>
+    /// <returns></returns>
+    public static dynamic Intersect (Region [] regions, dynamic rect)
+    {
+        dynamic? rBase = null;
+
+        foreach (Region r in regions)
+        {
+            if (r._rect.GetType ().Name == "Rectangle")
+            {
+                rBase = Rectangle.Intersect (rect, r._rect);
+
+                if ((Rectangle)rBase != Rectangle.Empty)
+                {
+                    break;
+                }
+            }
+            else
+            {
+                rBase = RectangleF.Intersect (rect, r._rect);
+
+                if ((RectangleF)rBase != RectangleF.Empty)
+                {
+                    break;
+                }
+            }
+
+        }
+
+        if (rBase is { })
+        {
+            return rBase;
+        }
+
+        if (rBase.GetType ().Name == "Rectangle")
+        {
+            return Rectangle.Empty;
+        }
+
+        return RectangleF.Empty;
+    }
+
+    /// <summary>
+    /// Calculates from the <see cref="Region"/> <see cref="HashSet{T}"/> to the intersection and the specified
+    /// <see cref="Rectangle"/> or <see cref="RectangleF"/> structure of each other.
+    /// </summary>
+    /// <param name="regions"></param>
+    /// <param name="rect"></param>
+    /// <returns></returns>
+    public static dynamic Intersect (HashSet<Region> regions, dynamic rect)
+    {
+        return Intersect (regions.ToArray (), rect);
+    }
+
+    /// <summary>
+    /// Updates this <see cref="Region"/> to the intersection of itself with the specified
+    /// <see cref="Rectangle"/> or <see cref="RectangleF"/> structure.
+    /// </summary>
+    /// <param name="rect"></param>
+    /// <returns></returns>
+    public dynamic Intersect (dynamic rect)
+    {
+        _rect = rect.GetType ().Name == "Rectangle" ? Rectangle.Intersect ((Rectangle)_rect, rect) : RectangleF.Intersect ((RectangleF)_rect, rect);
+
+        return _rect;
+    }
+
+    /// <summary>
+    /// Updates this <see cref="Region"/> to the intersection of itself with the specified <see cref="Region"/>.
+    /// </summary>
+    /// <param name="region"></param>
+    /// <returns></returns>
+    public dynamic Intersect (Region region)
+    {
+        return Intersect (region._rect);
+    }
+
+    #endregion
+}
---
+++ UnitTests/Drawing/RegionTests.cs
@@ -0,0 +1,252 @@
+﻿#nullable enable
+using Xunit.Abstractions;
+
+namespace Terminal.Gui.ViewTests;
+
+[Trait ("Category", "Output")]
+public class RegionTests (ITestOutputHelper output)
+{
+    [Fact]
+    public void HashSet_Does_Not_Allows_Duplicates ()
+    {
+        // Confirming Rectangle and RectangleF are equatable
+        Assert.True (Rectangle.Empty == RectangleF.Empty);
+        Rectangle rect = new Rectangle (10, 10, 10, 10);
+        RectangleF rectF = new RectangleF (10, 10, 10, 10);
+        Assert.True (rect == rectF);
+        rect.Offset (1, 1);
+        Assert.Equal (new RectangleF (11, 11, 10, 10), rect);
+        rectF.Offset (1, 1);
+        Assert.Equal (new Rectangle (11, 11, 10, 10), rectF);
+        rect.Inflate (1, 1);
+        Assert.Equal (new RectangleF (10, 10, 12, 12), rect);
+        rectF.Inflate (1, 1);
+        Assert.Equal (new Rectangle (10, 10, 12, 12), rectF);
+        HashSet<Region> regions = [new (new Rectangle (10, 10, 10, 10))];
+        Assert.True (regions.First ().Equals (new Region (new Rectangle (10, 10, 10, 10))));
+        Assert.False (regions.Add (new (new Rectangle (10, 10, 10, 10))));
+        Assert.Single (regions);
+    }
+
+    [Theory]
+    [MemberData (nameof (UnionDataWithArrays))]
+    public void Union_Array_Tests (Rectangle expected, Region [] regions)
+    {
+        Assert.Equal (expected, Region.Union (regions));
+    }
+
+    public static TheoryData<Rectangle, Region []> UnionDataWithArrays =>
+        new ()
+        {
+            {
+                new Rectangle (10, 10, 70, 70),
+                [
+                    new Region (new RectangleF (10, 10, 10, 10)),
+                    new Region (new Rectangle (30, 30, 10, 10)),
+                    new Region (new Rectangle (50, 50, 10, 10)),
+                    new Region (new Rectangle (70, 70, 10, 10))
+                ]
+            },
+            {
+                new Rectangle (-10, -10, 90, 90),
+                [
+                    new Region (new Rectangle (-10, -10, 10, 10)),
+                    new Region (new RectangleF (30, 30, 10, 10)),
+                    new Region (new Rectangle (50, 50, 10, 10)),
+                    new Region (new Rectangle (70, 70, 10, 10))
+                ]
+            }
+        };
+
+    [Theory]
+    [MemberData (nameof (UnionDataWithHashSet))]
+    public void Union_HashSet_Tests (Rectangle expected, HashSet<Region> regions)
+    {
+        Assert.Equal (expected, Region.Union (regions));
+    }
+
+    public static TheoryData<Rectangle, HashSet<Region>> UnionDataWithHashSet =>
+        new ()
+        {
+            {
+                new Rectangle (10, 10, 70, 70),
+                [
+                    new Region (new RectangleF (10, 10, 10, 10)),
+                    new Region (new Rectangle (30, 30, 10, 10)),
+                    new Region (new Rectangle (50, 50, 10, 10)),
+                    new Region (new Rectangle (70, 70, 10, 10))
+                ]
+            },
+            {
+                new Rectangle (-10, -10, 90, 90),
+                [
+                    new Region (new RectangleF (-10, -10, 10, 10)),
+                    new Region (new Rectangle (30, 30, 10, 10)),
+                    new Region (new Rectangle (50, 50, 10, 10)),
+                    new Region (new Rectangle (70, 70, 10, 10))
+                ]
+            }
+        };
+
+    [Fact]
+    public void Location_HashSet ()
+    {
+        HashSet<Region> regions = [new (new Rectangle (10, 10, 30, 30))];
+        Assert.Equal (new Point (10, 10), Region.Union (regions).Location);
+        regions = [new (new RectangleF (10, 10, 30, 30))];
+        Assert.Equal (new Point (10, 10), Region.Union (regions).Location);
+    }
+
+    [Fact]
+    public void Size_HashSet ()
+    {
+        HashSet<Region> regions = [new (new Rectangle (10, 10, 30, 30))];
+        Assert.Equal (new Size (30, 30), Region.Union (regions).Size);
+        regions = [new (new RectangleF (10, 10, 30, 30))];
+        Assert.Equal (new Size (30, 30), Region.Union (regions).Size);
+    }
+
+    [Fact]
+    public void ViewsGetBounds_Test ()
+    {
+        var subView1 = new View { Width = 10, Height = 10 };
+        var subView2 = new View { X = -1, Y = -1, Width = 20, Height = 20 };
+        var super = new View ();
+        super.Add (subView1, subView2);
+        super.BeginInit ();
+        super.EndInit ();
+        super.LayoutSubviews ();
+
+        Assert.Equal (new Rectangle (0, 0, 20, 20), Region.GetViewsBounds ([.. super.Subviews]));
+
+        subView2.ViewportSettings |= ViewportSettings.AllowNegativeX | ViewportSettings.AllowNegativeY;
+        subView2.Viewport = subView2.Frame;
+        Assert.Equal (new Rectangle (-1, -1, 20, 20), Region.GetViewsBounds ([.. super.Subviews]));
+    }
+
+    [Theory]
+    [MemberData (nameof (IntersectDataWithArrays))]
+    public void Intersect_Array_Tests (Region [] regions, Rectangle rect, Rectangle expected)
+    {
+        Assert.Equal (expected, Region.Intersect (regions, rect));
+    }
+
+    public static TheoryData<Region [], Rectangle, Rectangle> IntersectDataWithArrays =>
+        new ()
+        {
+            {
+                [
+                    new Region (new Rectangle (10, 10, 10, 10)),
+                    new Region (new RectangleF (30, 30, 10, 10)),
+                    new Region (new Rectangle (50, 50, 10, 10)),
+                    new Region (new Rectangle (70, 70, 10, 10))
+                ],
+                new Rectangle (10, 10, 70, 70),
+                new Rectangle (10, 10, 10, 10)
+            },
+            {
+                [
+                    new Region (new Rectangle (-10, -10, 10, 10)),
+                    new Region (new RectangleF (30, 30, 10, 10)),
+                    new Region (new Rectangle (50, 50, 10, 10)),
+                    new Region (new Rectangle (70, 70, 10, 10))
+                ],
+                new Rectangle (-10, -10, 90, 90),
+                new Rectangle (-10, -10, 10, 10)
+            },
+            {
+                [
+                    new Region (new Rectangle (10, 10, 70, 70)),
+                    new Region (new RectangleF (30, 30, 40, 40)),
+                    new Region (new Rectangle (50, 50, 20, 20)),
+                    new Region (new Rectangle (60, 60, 10, 10))
+                ],
+                new Rectangle (20, 20, 60, 60),
+                new Rectangle (20, 20, 60, 60)
+            },
+            {
+                [
+                    new Region (new Rectangle (-10, -10, 70, 70)),
+                    new Region (new RectangleF (30, 30, 40, 40)),
+                    new Region (new Rectangle (50, 50, 20, 20)),
+                    new Region (new Rectangle (60, 60, 10, 10))
+                ],
+                new Rectangle (20, 20, 60, 60),
+                new Rectangle (20, 20, 40, 40)
+            }
+        };
+
+    [Theory]
+    [MemberData (nameof (IntersectDataWithHashSet))]
+    public void Intersect_HashSet_Tests (HashSet<Region> regions, Rectangle rect, Rectangle expected)
+    {
+        Assert.Equal (expected, Region.Intersect (regions, rect));
+    }
+
+    public static TheoryData<HashSet<Region>, Rectangle, Rectangle> IntersectDataWithHashSet =>
+        new ()
+        {
+            {
+                [
+                    new Region (new RectangleF (10, 10, 10, 10)),
+                    new Region (new Rectangle (30, 30, 10, 10)),
+                    new Region (new Rectangle (50, 50, 10, 10)),
+                    new Region (new Rectangle (70, 70, 10, 10))
+                ],
+                new Rectangle (10, 10, 70, 70),
+                new Rectangle (10, 10, 10, 10)
+            },
+            {
+                [
+                    new Region (new RectangleF (-10, -10, 10, 10)),
+                    new Region (new Rectangle (30, 30, 10, 10)),
+                    new Region (new Rectangle (50, 50, 10, 10)),
+                    new Region (new Rectangle (70, 70, 10, 10))
+                ],
+                new Rectangle (-10, -10, 90, 90),
+                new Rectangle (-10, -10, 10, 10)
+            },
+            {
+                [
+                    new Region (new RectangleF (10, 10, 70, 70)),
+                    new Region (new Rectangle (30, 30, 40, 40)),
+                    new Region (new Rectangle (50, 50, 20, 20)),
+                    new Region (new Rectangle (60, 60, 10, 10))
+                ],
+                new Rectangle (20, 20, 60, 60),
+                new Rectangle (20, 20, 60, 60)
+            },
+            {
+                [
+                    new Region (new RectangleF (-10, -10, 70, 70)),
+                    new Region (new Rectangle (30, 30, 40, 40)),
+                    new Region (new Rectangle (50, 50, 20, 20)),
+                    new Region (new Rectangle (60, 60, 10, 10))
+                ],
+                new Rectangle (20, 20, 60, 60),
+                new Rectangle (20, 20, 40, 40)
+            }
+        };
+
+    [Fact]
+    public void Constructor_Defaults ()
+    {
+        var region = new Region ();
+        Assert.Equal (Rectangle.Empty, region.Union (Rectangle.Empty));
+
+        var region2 = new Region (new (0, 0, 10, 10));
+        Assert.Equal (new Rectangle  (0, 0, 10, 10), region.Union (region2));
+
+        Assert.Equal (new Rectangle (1, 1, 9, 9), region.Intersect (new Rectangle (1, 1, 20, 20)));
+
+        Assert.Equal (new Rectangle (1, 1, 9, 9), region.Intersect (region2));
+    }
+
+    [Fact]
+    public void GetHashCode_Test ()
+    {
+        var rect = Rectangle.Empty;
+        var region = new Region (rect);
+        Assert.Equal (HashCode.Combine (rect), region.GetHashCode ());
+    }
+}
