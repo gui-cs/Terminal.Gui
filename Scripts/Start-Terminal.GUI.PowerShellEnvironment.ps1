8cb748e45274382dcf186cd1320f0dc0e43e30d4
Brandon Thetford <github@snapsinazfs.com>
2024-04-26 21:23:04 -0700

    Reorganized, simplified, and standardized PowerShell modules.

--- Scripts/Start-Terminal.GUI.PowerShellEnvironment.ps1
+++
@@ -1,8 +0,0 @@
-<#
-  .SYNOPSIS
-  Start a Terminal.Gui.PowerShell environment and load all modules.
-  .DESCRIPTION
-  Starts a new Terminal.Gui.PowerShell environment, with all modules imported.
-#>
-
-. ./Load-Module.ps1
