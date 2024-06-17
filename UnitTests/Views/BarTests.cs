d6933234b5e164db5a3a8826041dce139466cf1b
Tig <tig@users.noreply.github.com>
2024-06-17 08:00:06 -0700

    Added BarTests

---
+++ UnitTests/Views/BarTests.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+
+namespace Terminal.Gui.ViewsTests;
+
+[TestSubject (typeof (Bar))]
+public class BarTests
+{
+    [Fact]
+    public void Constructor_Defaults ()
+    {
+        var bar = new Bar ();
+
+        Assert.NotNull (bar);
+        Assert.True (bar.CanFocus);
+        Assert.IsType<DimAuto> (bar.Width);
+        Assert.IsType<DimAuto> (bar.Height);
+
+        // TOOD: more
+    }
+
+}
