266cb0a6b95b87d70ed0342a832644f6357e45a8
Tig <tig@users.noreply.github.com>
2024-06-26 10:30:52 -0700

    Initial commit. Added interface

---
+++ Terminal.Gui/View/ISupportsDesignMode.cs
@@ -0,0 +1,13 @@
+﻿namespace Terminal.Gui;
+
+/// <summary>
+/// Indicates that the view supports design mode.
+/// </summary>
+public interface ISupportsDesignMode
+{
+    /// <summary>
+    /// Call this to tell the View to load "demo data"
+    /// </summary>
+    /// <returns><see langword="true"/> if the view succesfully loaded demo data.</returns>
+    public bool LoadDemoData ();
+}
