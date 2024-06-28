57145ec258a55845a86069e511819fc8196b13f6
Tig <tig@users.noreply.github.com>
2024-06-28 08:48:24 -0700

    Highlight stuff to distinct files

---
+++ Terminal.Gui/View/HighlightEventArgs.cs
@@ -0,0 +1,10 @@
+﻿namespace Terminal.Gui;
+
+/// <summary>
+/// Event arguments for the <see cref="View.Highlight"/> event.
+/// </summary>
+public class HighlightEventArgs : CancelEventArgs<HighlightStyle>
+{
+    /// <inheritdoc />
+    public HighlightEventArgs (HighlightStyle currentValue, HighlightStyle newValue) : base (currentValue, newValue) { }
+}
