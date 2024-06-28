5c7a177bddcca005e678a14f8c698435ea61a573
Tig <tig@users.noreply.github.com>
2024-06-28 08:49:13 -0700

    Code cleanup

--- UnitTests/View/TitleTests.cs
+++ UnitTests/View/TitleTests.cs
@@ -5,6 +5,43 @@ namespace Terminal.Gui.ViewTests;
 
 public class TitleTests (ITestOutputHelper output)
 {
+    [SetupFakeDriver]
+    [Fact]
+    public void Change_View_Size_Update_Title_Size ()
+    {
+        var view = new View
+        {
+            Title = "_Hello World",
+            Width = Dim.Auto (),
+            Height = Dim.Auto (),
+            BorderStyle = LineStyle.Single
+        };
+        var top = new Toplevel ();
+        top.Add (view);
+        top.BeginInit ();
+        top.EndInit ();
+
+        Assert.Equal (string.Empty, view.Text);
+        Assert.Equal (new (2, 2), view.Frame.Size);
+        top.Draw ();
+
+        TestHelpers.AssertDriverContentsWithFrameAre (
+                                                      @"
+┌┐
+└┘",
+                                                      output);
+
+        var text = "This text will increment the view size and display the title.";
+        view.Text = text;
+        top.Draw ();
+        Assert.Equal (text, view.Text);
+
+        // SetupFakeDriver only create a screen with 25 cols and 25 rows
+        Assert.Equal (new (text.Length, 1), view.GetContentSize ());
+
+        top.Dispose ();
+    }
+
     [Fact]
     public void Set_Title_Fires_TitleChanged ()
     {
@@ -68,42 +105,5 @@ public class TitleTests (ITestOutputHelper output)
         var view = new View { HotKeySpecifier = (Rune)'_', Title = "_Hello World" };
 
         Assert.Equal (Key.H, view.HotKey);
-    }
-
-    [SetupFakeDriver]
-    [Fact]
-    public void Change_View_Size_Update_Title_Size ()
-    {
-        var view = new View
-        {
-            Title = "_Hello World",
-            Width = Dim.Auto (),
-            Height = Dim.Auto (),
-            BorderStyle = LineStyle.Single
-        };
-        var top = new Toplevel ();
-        top.Add (view);
-        top.BeginInit ();
-        top.EndInit ();
-
-        Assert.Equal (string.Empty, view.Text);
-        Assert.Equal (new (2, 2), view.Frame.Size);
-        top.Draw ();
-
-        TestHelpers.AssertDriverContentsWithFrameAre (
-                                                      @"
-┌┐
-└┘",
-                                                      output);
-
-        var text = "This text will increment the view size and display the title.";
-        view.Text = text;
-        top.Draw ();
-        Assert.Equal (text, view.Text);
-
-        // SetupFakeDriver only create a screen with 25 cols and 25 rows
-        Assert.Equal (new (text.Length, 1), view.GetContentSize ());
-
-        top.Dispose ();
     }
 }
