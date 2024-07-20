d003d00ae059059a7de77029c6ebd3292c5f2cb9
Tig <tig@users.noreply.github.com>
2024-07-20 17:48:38 -0600

    Removed verbatim strings

--- UICatalog/UICatalog.cs
+++ UICatalog/UICatalog.cs
@@ -79,16 +79,17 @@ public class UICatalogApp
     /// <returns></returns>
     public static string GetAboutBoxMessage ()
     {
+        // NOTE: Do not use verbatim string here.
+        // WSL gets all confused.
         StringBuilder msg = new ();
-        msg.AppendLine ($"UI Catalog: A comprehensive sample library for");
+        msg.AppendLine ("UI Catalog: A comprehensive sample library for");
         msg.AppendLine ();
-        msg.AppendLine (
-@" _______                  _             _   _____       _
-|__   __|                (_)           | | / ____|     (_)
-   | | ___ _ __ _ __ ___  _ _ __   __ _| || |  __ _   _ _
-   | |/ _ \ '__| '_ ` _ \| | '_ \ / _` | || | |_ | | | | |
-   | |  __/ |  | | | | | | | | | | (_| | || |__| | |_| | |
-   |_|\___|_|  |_| |_| |_|_|_| |_|\__,_|_(_)_____|\__,_|_|");
+        msg.AppendLine (@" _______                  _             _   _____       _");
+        msg.AppendLine (@"|__   __|                (_)           | | / ____|     (_)");
+        msg.AppendLine (@"   | | ___ _ __ _ __ ___  _ _ __   __ _| || |  __ _   _ _");
+        msg.AppendLine (@"   | |/ _ \ '__| '_ ` _ \| | '_ \ / _` | || | |_ | | | | |");
+        msg.AppendLine (@"   | |  __/ |  | | | | | | | | | | (_| | || |__| | |_| | |");
+        msg.AppendLine (@"   |_|\___|_|  |_| |_| |_|_|_| |_|\__,_|_(_)_____|\__,_|_|");
         msg.AppendLine ();
         msg.AppendLine (@"v2 - Pre-Alpha");
         msg.AppendLine ();
