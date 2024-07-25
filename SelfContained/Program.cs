88954b9ab772fdeb7dd339a9be8c23512a96128a
BDisp <bd.bdisp@gmail.com>
2024-07-25 18:04:46 +0100

    Fix WSL InvariantCulture test.

--- SelfContained/Program.cs
+++ SelfContained/Program.cs
@@ -16,7 +16,7 @@ public static class Program
 
         #region The code in this region is not intended for use in a self-contained single-file. It's just here to make sure there is no functionality break with localization in Terminal.Gui using single-file
 
-        if (Equals (Thread.CurrentThread.CurrentUICulture, CultureInfo.InvariantCulture))
+        if (Equals (Thread.CurrentThread.CurrentUICulture, CultureInfo.InvariantCulture) && Application.SupportedCultures.Count == 0)
         {
             Debug.Assert (Application.SupportedCultures.Count == 0);
         }
