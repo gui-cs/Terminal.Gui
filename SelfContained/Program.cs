f3146a25cdab041a22d5a9c6986f9371dba91468
BDisp <bd.bdisp@gmail.com>
2024-08-01 10:47:31 +0100

    Re-add assertions to ensure it's working running the executable file.

--- SelfContained/Program.cs
+++ SelfContained/Program.cs
@@ -21,6 +21,11 @@ public static class Program
             // Only happens if the project has <InvariantGlobalization>true</InvariantGlobalization>
             Debug.Assert (Application.SupportedCultures.Count == 0);
         }
+        else
+        {
+            Debug.Assert (Application.SupportedCultures.Count > 0);
+            Debug.Assert (Equals (CultureInfo.CurrentCulture, Thread.CurrentThread.CurrentUICulture));
+        }
 
         #endregion
 
