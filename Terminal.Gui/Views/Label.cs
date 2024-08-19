3c1f9adc25c2c18c539a13f02f58a6a2263e17ba
Tig <tig@users.noreply.github.com>
2024-08-19 09:33:28 -0600

    Fixed Label warning

--- Terminal.Gui/Views/Label.cs
+++ Terminal.Gui/Views/Label.cs
@@ -51,7 +51,7 @@ public class Label : View
         set => TextFormatter.HotKeySpecifier = base.HotKeySpecifier = value;
     }
 
-    private new bool? FocusNext ()
+    private bool? FocusNext ()
     {
         var me = SuperView?.Subviews.IndexOf (this) ?? -1;
         if (me != -1 && me < SuperView?.Subviews.Count - 1)
