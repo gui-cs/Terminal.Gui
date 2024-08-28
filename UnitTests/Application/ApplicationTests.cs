c6c393699c8aa7dc635457c8e4391f8122eee902
Tig <tig@users.noreply.github.com>
2024-08-27 23:25:53 -0700

    Debugging DatePickers fail 5

--- UnitTests/Application/ApplicationTests.cs
+++ UnitTests/Application/ApplicationTests.cs
@@ -463,7 +463,7 @@ public class ApplicationTests
     }
 
     [Fact]
-    public void InitWithoutTopLevelFactory_Begin_End_Cleans_Up ()
+    public void Init_WithoutTopLevelFactory_Begin_End_Cleans_Up ()
     {
         // Begin will cause Run() to be called, which will call Begin(). Thus will block the tests
         // if we don't stop
@@ -505,6 +505,15 @@ public class ApplicationTests
         Assert.Null (Application.Top);
         Assert.Null (Application.MainLoop);
         Assert.Null (Application.Driver);
+    }
+
+    [Fact]
+    public void Init_NoParam_ForceDriver_Works ()
+    {
+        Application.ForceDriver = "FakeDriver";
+        Application.Init ();
+        Assert.IsType<FakeDriver> (Application.Driver);
+        Application.ResetState ();
     }
 
     [Fact]
