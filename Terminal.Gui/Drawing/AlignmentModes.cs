13a41262aa71b636e060ee8e17b7af55ade2dc0c
Tig <tig@users.noreply.github.com>
2024-09-18 16:42:43 -0600

    Updated docfx template

--- Terminal.Gui/Drawing/AlignmentModes.cs
+++ Terminal.Gui/Drawing/AlignmentModes.cs
@@ -21,27 +21,29 @@ public enum AlignmentModes
 
     /// <summary>
     ///     At least one space will be added between items. Useful for justifying text where at least one space is needed.
+    /// </summary>
+    /// <remarks>
     ///     <para>
     ///         If the total size of the items is greater than the container size, the space between items will be ignored
     ///         starting from the end.
     ///     </para>
-    /// </summary>
+    /// </remarks>
     AddSpaceBetweenItems = 2,
 
     /// <summary>
-    ///     When aligning via <see cref="Alignment.Start"/> or <see cref="Alignment.End"/>, the item opposite to the alignment
-    ///     (the first or last item) will be ignored.
+    ///    When aligning via <see cref="Alignment.Start"/> or <see cref="Alignment.End"/>, the item opposite to the alignment (the first or last item) will be ignored.
+    /// </summary>
+    /// <remarks>
     ///     <para>
     ///         If the container is smaller than the total size of the items, the end items will be clipped (their locations
     ///         will be greater than the container size).
     ///     </para>
-    ///     ///
-    ///     <example>
-    ///         <c>
-    ///             Start: |111 2222     33333|
-    ///             End:   |111     2222 33333|
-    ///         </c>
-    ///     </example>
-    /// </summary>
-    IgnoreFirstOrLast = 4
+    /// </remarks>
+    /// <example>
+    ///     <c>
+    ///         Start: |111 2222     33333|
+    ///         End:   |111     2222 33333|
+    ///     </c>
+    /// </example>
+    IgnoreFirstOrLast = 4,
 }
