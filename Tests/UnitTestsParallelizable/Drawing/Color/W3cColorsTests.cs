a2d40f33b85479c922ba52d02538279436170b5e
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2025-03-25 19:17:20 +0200

    Change W3C colors tests to be similar to name resolvers

--- Tests/UnitTestsParallelizable/Drawing/Color/W3cColorsTests.cs
+++ Tests/UnitTestsParallelizable/Drawing/Color/W3cColorsTests.cs
@@ -13,7 +13,6 @@ public class W3cColorsTests
     }
 
     [Theory]
-    // TODO: Solve conflicts with ColorName16
     [InlineData (nameof (W3cColor.Aqua))]
     [InlineData (nameof (W3cColor.Cyan))]
     [InlineData (nameof (W3cColor.DarkGray))]
@@ -38,49 +37,112 @@ public class W3cColorsTests
     }
 
     [Theory]
-    [InlineData (240, 248, 255, true, nameof(W3cColor.AliceBlue))]
-    [InlineData (0, 255, 255, true, nameof(W3cColor.Aqua))]
-    [InlineData (255, 0, 0, true, nameof(W3cColor.Red))]
-    [InlineData (0, 128, 0, true, nameof(W3cColor.Green))]
-    [InlineData (0, 0, 255, true, nameof(W3cColor.Blue))]
-    [InlineData (0, 255, 0, true, nameof(W3cColor.Lime))]
-    [InlineData (0, 0, 0, true, nameof(W3cColor.Black))]
-    [InlineData (255, 255, 255, true, nameof(W3cColor.White))]
-    [InlineData (154, 205, 50, true, nameof(W3cColor.YellowGreen))]
-    [InlineData (1, 2, 3, false, null)]
-    public void TryNameColor_ReturnsExpectedColorName(int r, int g, int b, bool expectedSuccess, string? expectedName)
+    [InlineData (240, 248, 255, nameof (W3cColor.AliceBlue))]
+    [InlineData (0, 255, 255, nameof (W3cColor.Aqua))]
+    [InlineData (255, 0, 0, nameof (W3cColor.Red))]
+    [InlineData (0, 128, 0, nameof (W3cColor.Green))]
+    [InlineData (0, 0, 255, nameof (W3cColor.Blue))]
+    [InlineData (0, 255, 0, nameof (W3cColor.Lime))]
+    [InlineData (0, 0, 0, nameof (W3cColor.Black))]
+    [InlineData (255, 255, 255, nameof (W3cColor.White))]
+    [InlineData (154, 205, 50, nameof (W3cColor.YellowGreen))]
+    public void TryNameColor_ReturnsExpectedColorName (int r, int g, int b, string expectedName)
     {
+        var expected = (true, expectedName);
+
         Color inputColor = new(r, g, b);
         bool actualSuccess = W3cColors.TryNameColor (inputColor, out string? actualName);
+        var actual = (actualSuccess, actualName);
 
-        Assert.Equal ((expectedSuccess, expectedName), (actualSuccess, actualName));
+        Assert.Equal (expected, actual);
+    }
+
+    [Fact]
+    public void TryNameColor_NoMatchFails ()
+    {
+        (bool, string?) expected = (false, null);
+
+        bool actualSuccess = W3cColors.TryNameColor (new Color (1, 2, 3), out string? actualName);
+        var actual = (actualSuccess, actualName);
+
+        Assert.Equal (expected, actual);
     }
 
     [Theory]
-    [InlineData ("Red", true, 255, 0, 0)]
-    [InlineData ("red", true, 255, 0, 0)]
-    [InlineData ("RED", true, 255, 0, 0)]
-    [InlineData ("Green", true, 0, 128, 0)]
-    [InlineData ("green", true, 0, 128, 0)]
-    [InlineData ("GREEN", true, 0, 128, 0)]
-    [InlineData ("Blue", true, 0, 0, 255)]
-    [InlineData ("blue", true, 0, 0, 255)]
-    [InlineData ("BLUE", true, 0, 0, 255)]
-    [InlineData ("Nada", false, 0, 0, 0)]
+    [InlineData (nameof (W3cColor.AliceBlue), 240, 248, 255)]
+    [InlineData (nameof (W3cColor.BlanchedAlmond), 255, 235, 205)]
+    [InlineData (nameof (W3cColor.CadetBlue), 95, 158, 160)]
+    [InlineData (nameof (W3cColor.DarkBlue), 0, 0, 139)]
+    [InlineData (nameof (W3cColor.FireBrick), 178, 34, 34)]
+    [InlineData (nameof (W3cColor.Gainsboro), 220, 220, 220)]
+    [InlineData (nameof (W3cColor.HoneyDew), 240, 255, 240)]
+    [InlineData (nameof (W3cColor.Indigo), 75, 0, 130)]
+    [InlineData (nameof (W3cColor.Khaki), 240, 230, 140)]
+    [InlineData (nameof (W3cColor.Lavender), 230, 230, 250)]
+    [InlineData (nameof (W3cColor.Maroon), 128, 0, 0)]
+    [InlineData (nameof (W3cColor.Navy), 0, 0, 128)]
+    [InlineData (nameof (W3cColor.Olive), 128, 128, 0)]
+    [InlineData (nameof (W3cColor.Plum), 221, 160, 221)]
+    [InlineData (nameof (W3cColor.RoyalBlue), 65, 105, 225)]
+    [InlineData (nameof (W3cColor.Silver), 192, 192, 192)]
+    [InlineData (nameof (W3cColor.Tomato), 255, 99, 71)]
+    [InlineData (nameof (W3cColor.Violet), 238, 130, 238)]
+    [InlineData (nameof (W3cColor.WhiteSmoke), 245, 245, 245)]
+    [InlineData (nameof (W3cColor.YellowGreen), 154, 205, 50)]
     // Aliases also work
-    // TODO: Solve conflicts with ColorName16
-    [InlineData (nameof(W3cColor.Aqua), true, 0, 255, 255)]
-    [InlineData (nameof(W3cColor.Cyan), true, 0, 255, 255)]
-    [InlineData (nameof(W3cColor.DarkGray), true, 169, 169, 169)]
-    [InlineData (nameof(W3cColor.DarkGrey), true, 169, 169, 169)]
-    public void TryParseColor_ReturnsExpectedColor(string inputName, bool expectedSuccess, int r, int g, int b)
+    [InlineData (nameof (W3cColor.Aqua), 0, 255, 255)]
+    [InlineData (nameof (W3cColor.Cyan), 0, 255, 255)]
+    [InlineData (nameof (W3cColor.DarkGray), 169, 169, 169)]
+    [InlineData (nameof (W3cColor.DarkGrey), 169, 169, 169)]
+    // Case-insensitive
+    [InlineData ("Red", 255, 0, 0)]
+    [InlineData ("red", 255, 0, 0)]
+    [InlineData ("RED", 255, 0, 0)]
+    public void TryParseColor_ReturnsExpectedColor (string inputName, int r, int g, int b)
     {
-        Color expectedColor = expectedSuccess
-            ? new (r, g, b)
-            : default;
+        var expected = (true, new Color(r, g, b));
 
         bool actualSuccess = W3cColors.TryParseColor (inputName, out Color actualColor);
+        var actual = (actualSuccess, actualColor);
 
-        Assert.Equal ((expectedSuccess, expectedColor), (actualSuccess, actualColor));
+        Assert.Equal (expected, actual);
+    }
+
+    [Theory]
+    [InlineData ("16737095", 255, 99, 71)] // W3cColor.Tomato
+    public void TryParseColor_ResolvesValidEnumNumber (string inputName, byte r, byte g, byte b)
+    {
+        var expected = (true, new Color(r, g, b));
+
+        bool actualSuccess = W3cColors.TryParseColor (inputName, out Color actualColor);
+        var actual = (actualSuccess, actualColor);
+
+        Assert.Equal (expected, actual);
+    }
+
+    [Theory]
+    [InlineData (null)]
+    [InlineData ("")]
+    [InlineData ("brightlight")]
+    public void TryParseColor_FailsOnInvalidColorName (string? invalidName)
+    {
+        var expected = (false, default(Color));
+
+        bool actualSuccess = W3cColors.TryParseColor (invalidName, out Color actualColor);
+        var actual = (actualSuccess, actualColor);
+
+        Assert.Equal (expected, actual);
+    }
+
+    [Theory]
+    [InlineData ("-16737095")]
+    public void TryParseColor_FailsOnInvalidEnumNumber (string invalidName)
+    {
+        var expected = (false, default(Color));
+
+        bool actualSuccess = W3cColors.TryParseColor (invalidName, out Color actualColor);
+        var actual = (actualSuccess, actualColor);
+
+        Assert.Equal (expected, actual);
     }
 }
