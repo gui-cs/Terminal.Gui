0c61f38a296343815c5c4bf38137e14c920e5273
Tig <tig@users.noreply.github.com>
2025-04-01 19:54:45 -0600

    unit test fix

--- Tests/UnitTestsParallelizable/Views/MenuBarItemTests.cs
+++ Tests/UnitTestsParallelizable/Views/MenuBarItemTests.cs
@@ -10,22 +10,13 @@ public class MenuBarItemTests ()
     public void Constructors_Defaults ()
     {
         var menuBarItem = new MenuBarItemv2 ();
-        Assert.Empty (menuBarItem.SubViews);
         Assert.Null (menuBarItem.PopoverMenu);
         Assert.Null (menuBarItem.TargetView);
 
         menuBarItem = new MenuBarItemv2 (targetView: null, command: Command.NotBound, commandText: null, popoverMenu: null);
-        Assert.Empty (menuBarItem.SubViews);
         Assert.Null (menuBarItem.PopoverMenu);
         Assert.Null (menuBarItem.TargetView);
 
 
-    }
-
-    [Fact]
-    public void Add_With_No_Root_Throws ()
-    {
-        var menuBarItem = new MenuBarItemv2 ();
-        Assert.Throws<InvalidOperationException> (() => menuBarItem.Add (new MenuItemv2 ()));
     }
 }
