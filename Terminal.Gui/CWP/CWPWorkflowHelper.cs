eab95e162e6ddb4dad48d57510ddb80561f695e8
Tig <tig@users.noreply.github.com>
2025-05-16 16:11:16 -0600

    Added prototype code

---
+++ Terminal.Gui/CWP/CWPWorkflowHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terminal.Gui.CWP;
+class CWPWorkflowHelper
+{
+}
