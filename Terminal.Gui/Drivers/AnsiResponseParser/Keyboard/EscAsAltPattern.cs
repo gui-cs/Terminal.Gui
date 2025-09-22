bbd70217b8e6f4ccb75344189def36e50b32c2cb
BDisp <bd.bdisp@gmail.com>
2025-09-22 13:04:24 +0100

    Fix regex pattern

--- Terminal.Gui/Drivers/AnsiResponseParser/Keyboard/EscAsAltPattern.cs
+++ Terminal.Gui/Drivers/AnsiResponseParser/Keyboard/EscAsAltPattern.cs
@@ -8,7 +8,7 @@ internal class EscAsAltPattern : AnsiKeyboardParserPattern
     public EscAsAltPattern () { IsLastMinute = true; }
 
 #pragma warning disable IDE1006 // Naming Styles
-    private static readonly Regex _pattern = new (@"^\u001b([\u0001-\u001a\u001f\a-zA-Z0-9_])$");
+    private static readonly Regex _pattern = new (@"^\u001b([\u0001-\u001a\u001fa-zA-Z0-9_])$");
 #pragma warning restore IDE1006 // Naming Styles
 
     public override bool IsMatch (string? input) { return _pattern.IsMatch (input!); }
