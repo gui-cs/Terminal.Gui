75743d89cffb7768943a892b429178bbbee438fc
Tig <tig@users.noreply.github.com>
2025-10-30 09:45:05 -0600

    Refactor ArrangementEditor event handling
    Removed the `ArrangementFlagsOnValueChanged` method, which previously handled updates to `ViewToEdit` properties based on arrangement flags. Updated `ArrangementEditor_Initialized` to attach the event handler to `_arrangementSelector.ValueChanged`. The logic for handling arrangement changes has been refactored or relocated.

--- Examples/UICatalog/Scenarios/EditorsAndHelpers/ArrangementEditor.cs
+++ Examples/UICatalog/Scenarios/EditorsAndHelpers/ArrangementEditor.cs
@@ -72,32 +72,4 @@ public sealed class ArrangementEditor : EditorBase
     {
         _arrangementSelector.ValueChanged += ArrangementFlagsOnValueChanged;
     }
-
-    private void ArrangementFlagsOnValueChanged (object? sender, EventArgs<uint?> e)
-    {
-        if (ViewToEdit is { } && e.Value is {})
-        {
-            ViewToEdit.Arrangement = (ViewArrangement)e.Value;
-
-            if (ViewToEdit.Arrangement.HasFlag (ViewArrangement.Overlapped))
-            {
-                ViewToEdit.ShadowStyle = ShadowStyle.Transparent;
-                ViewToEdit.SchemeName = "Toplevel";
-            }
-            else
-            {
-                ViewToEdit.ShadowStyle = ShadowStyle.None;
-                ViewToEdit.SchemeName = ViewToEdit!.SuperView!.SchemeName;
-            }
-
-            if (ViewToEdit.Arrangement.HasFlag (ViewArrangement.Movable))
-            {
-                ViewToEdit.BorderStyle = LineStyle.Double;
-            }
-            else
-            {
-                ViewToEdit.BorderStyle = LineStyle.Single;
-            }
-        }
-    }
 }
