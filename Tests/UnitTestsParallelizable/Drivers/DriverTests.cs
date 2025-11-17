af7f7ad478a41313e803ccf6aed15436a67443d0
BDisp <bd.bdisp@gmail.com>
2025-11-17 14:26:23 +0000

    Moving test to Application folder

--- Tests/UnitTestsParallelizable/Drivers/DriverTests.cs
+++ Tests/UnitTestsParallelizable/Drivers/DriverTests.cs
@@ -2,42 +2,4 @@
 
 namespace UnitTests_Parallelizable.DriverTests;
 
-public class DriverTests : FakeDriverBase
-{
-    [Fact]
-    public void ForceDriver_Does_Not_Changes_If_It_Has_Valid_Value ()
-    {
-        IDriver driver = CreateFakeDriver ();
-
-        Assert.False (Application.Initialized);
-        Assert.Null (Application.Driver);
-        Assert.Equal (string.Empty, Application.ForceDriver);
-
-        Application.ForceDriver = "fake";
-        Assert.Equal ("fake", Application.ForceDriver);
-
-        Application.ForceDriver = "dotnet";
-        Assert.Equal ("fake", Application.ForceDriver);
-    }
-
-    [Fact]
-    public void ForceDriver_Throws_If_Initialized_Changed_To_Another_Value ()
-    {
-        IDriver driver = CreateFakeDriver ();
-
-        Assert.False (Application.Initialized);
-        Assert.Null (Application.Driver);
-        Assert.Equal (string.Empty, Application.ForceDriver);
-
-        Application.Init (driver);
-        Assert.True (Application.Initialized);
-        Assert.NotNull (Application.Driver);
-        Assert.Equal ("fake", Application.Driver.GetName ());
-        Assert.Equal (string.Empty, Application.ForceDriver);
-
-        Assert.Throws<InvalidOperationException> (() => Application.ForceDriver = "dotnet");
-
-        Application.ForceDriver = "fake";
-        Assert.Equal ("fake", Application.ForceDriver);
-    }
-}
+public class DriverTests : FakeDriverBase { }
