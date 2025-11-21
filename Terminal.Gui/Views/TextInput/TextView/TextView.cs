26b3fc916db08fc5e5a9738e84f830d239111787
Tig <tig@users.noreply.github.com>
2025-11-21 15:59:40 -0700

    Extract TextViewAutocomplete to separate file
    Move TextViewAutocomplete class from TextView.cs to TextViewAutocomplete.cs
    
    Build and all 163 tests pass

--- Terminal.Gui/Views/TextInput/TextView/TextView.cs
+++ Terminal.Gui/Views/TextInput/TextView/TextView.cs
@@ -4885,23 +4885,3 @@ public class TextView : View, IDesignable
         base.Dispose (disposing);
     }
 }
-
-/// <summary>
-///     Renders an overlay on another view at a given point that allows selecting from a range of 'autocomplete'
-///     options. An implementation on a TextView.
-/// </summary>
-public class TextViewAutocomplete : PopupAutocomplete
-{
-    /// <inheritdoc/>
-    protected override void DeleteTextBackwards () { ((TextView)HostControl!).DeleteCharLeft (); }
-
-    /// <inheritdoc/>
-    protected override void InsertText (string accepted) { ((TextView)HostControl!).InsertText (accepted); }
-
-    /// <inheritdoc/>
-    protected override void SetCursorPosition (int column)
-    {
-        ((TextView)HostControl!).CursorPosition =
-            new (column, ((TextView)HostControl).CurrentRow);
-    }
-}
