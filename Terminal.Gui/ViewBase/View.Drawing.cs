8f217187c6a1aff40f81b8728230f8403aafc8cf
Tig <tig@users.noreply.github.com>
2026-03-28 15:57:04 -0600

    Fixed?

--- Terminal.Gui/ViewBase/View.Drawing.cs
+++ Terminal.Gui/ViewBase/View.Drawing.cs
@@ -466,7 +466,7 @@ public partial class View // Drawing APIs
 
         Driver.FillRect (toClear);
 
-        // context.AddDrawnRectangle (toClear);
+        context?.AddDrawnRectangle (toClear);
 
         SetNeedsDraw ();
     }
